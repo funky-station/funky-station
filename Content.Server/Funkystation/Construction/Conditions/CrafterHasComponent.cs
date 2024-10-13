f55efbe8bcc05152d24fc8380c5c4a92ae21666b
taydeo <td12233a@gmail.com>
2024-10-12 21:57:16 -0500

    testing

--- Content.Server/Funkystation/Construction/Conditions/CrafterHasComponent.cs
+++ Content.Server/Funkystation/Construction/Conditions/CrafterHasComponent.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Construction;
 using Content.Shared.Examine;
+using Content.Shared.Mind;
 using Content.Shared.Revolutionary.Components;
 using JetBrains.Annotations;
 using Robust.Shared.Prototypes;
@@ -9,8 +10,16 @@ namespace Content.Server.Construction.Conditions;
 
 [UsedImplicitly]
 [DataDefinition]
+/// <summary>
+/// A graph condition to see if a crafter has a certain component. Useful if you want certain
+/// recipes only accessible by "knowledge" or some other mechanic. Only works if the item
+/// is parented to their character, which, I know, drawback.
+/// </summary>
 public sealed partial class CrafterHasComponent : IGraphCondition
 {
+    [Dependency]
+    private readonly SharedMindSystem _mind = default!;
+
     [DataField("componentName")]
     public EntProtoId ComponentName;
 
@@ -21,7 +30,26 @@ public sealed partial class CrafterHasComponent : IGraphCondition
 
     public bool Condition(EntityUid uid, IEntityManager entityManager)
     {
-        return true;
+        var transformComponent = entityManager.GetComponentOrNull<TransformComponent>(uid);
+
+        if (transformComponent == null)
+            return false;
+
+        if (!_mind.TryGetMind(transformComponent.ParentUid, out var mindId, out var mindComp))
+            return false;
+
+        if (mindComp.CurrentEntity == null)
+            return false;
+
+        foreach (var comp in entityManager.GetComponents((EntityUid) mindComp.CurrentEntity))
+        {
+            if (comp.GetType().ToString() == ComponentName)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public bool DoExamine(ExaminedEvent args)

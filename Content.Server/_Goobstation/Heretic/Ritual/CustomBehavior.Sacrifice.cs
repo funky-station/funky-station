df0e4636297a0746572b9e6ee38540e897e00149
John Space <bigdumb421@gmail.com>
2024-10-21 13:28:59 -0700

    Better Sacrifice Teleporting (#54)

--- Content.Server/_Goobstation/Heretic/Ritual/CustomBehavior.Sacrifice.cs
+++ Content.Server/_Goobstation/Heretic/Ritual/CustomBehavior.Sacrifice.cs
@@ -9,6 +9,7 @@ using Content.Shared.Damage;
 using Content.Shared.Heretic;
 using Content.Server.Heretic.EntitySystems;
 using Content.Server.Chat.Managers;
+using Content.Server.Atmos.EntitySystems;
 using Robust.Shared.Random;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Mobs;
@@ -17,6 +18,7 @@ using Content.Shared.Inventory;
 using Robust.Server.GameObjects;
 using Content.Shared.Chat;
 using System.Linq;
+using Robust.Shared.Physics;
 
 namespace Content.Server.Heretic.Ritual;
 
@@ -199,8 +201,9 @@ public partial class RitualSacrificeBehavior : RitualCustomBehavior
         var xformSystem = args.EntityManager.System<TransformSystem>();
         var randomSystem = IoCManager.Resolve<IRobustRandom>();
         var sharedXformSystem = args.EntityManager.System<SharedTransformSystem>();
+        var atmosSystem = args.EntityManager.System<AtmosphereSystem>();
 
-        var maxrandomtp = 40; // this is how many attempts it will try before breaking the loop -space
+        var maxrandomtp = 50; // this is how many attempts it will try before breaking the loop -space
         var maxrandomradius = 40; // this is the max range it will do -space
 
         if (!args.EntityManager.TryGetComponent<TransformComponent>(uid, out var transformComponent))
@@ -213,11 +216,14 @@ public partial class RitualSacrificeBehavior : RitualCustomBehavior
             var randVector = randomSystem.NextVector2(maxrandomradius);
             newCoords = coords.Offset(randVector);
 
+            xformSystem.SetCoordinates(uid, newCoords); //move person teleported to check if they're under a tile (the tiles intersecting doesnt account for this so we need to do this) -space
+
+            var air = atmosSystem.GetContainingMixture((uid, transformComponent)); //check if the room has any sort of atmos (this prevents getting teleported into solars and grilles outta the station) -space
+
             // if they're not in space and not in wall, it will choose these coords and end the loop -space
-            if (transformComponent.GridUid != null && !lookupSystem.GetEntitiesIntersecting(newCoords.ToMap(args.EntityManager, sharedXformSystem), LookupFlags.Static).Any())
+            if (transformComponent.GridUid != null && air != null && !lookupSystem.GetEntitiesIntersecting(newCoords.ToMap(args.EntityManager, sharedXformSystem), LookupFlags.Static).Any())
                 break;
         }
 
-        xformSystem.SetCoordinates(uid, newCoords);
     }
 }

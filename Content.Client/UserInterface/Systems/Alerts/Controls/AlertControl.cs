6fd4705573782acde34e5016156155704be82583
Skye <57879983+Rainbeon@users.noreply.github.com>
2024-12-12 20:45:54 -0500

    Port Dynamic Message Alerts + add to Borg Battery Alert (#174)

--- Content.Client/UserInterface/Systems/Alerts/Controls/AlertControl.cs
+++ Content.Client/UserInterface/Systems/Alerts/Controls/AlertControl.cs
@@ -30,7 +30,21 @@ namespace Content.Client.UserInterface.Systems.Alerts.Controls
             }
         }
 
+        public string? DynamicMessage
+        {
+            get => _dynamicMessage;
+            set
+            {
+                _dynamicMessage = value;
+                if (SuppliedTooltip is ActionAlertTooltip actionAlertTooltip)
+                {
+                    actionAlertTooltip.DynamicMessage = value;
+                }
+            }
+        }
+
         private (TimeSpan Start, TimeSpan End)? _cooldown;
+        private string? _dynamicMessage;
 
         private short? _severity;
         private readonly IGameTiming _gameTiming;
@@ -71,7 +85,7 @@ namespace Content.Client.UserInterface.Systems.Alerts.Controls
         {
             var msg = FormattedMessage.FromMarkupOrThrow(Loc.GetString(Alert.Name));
             var desc = FormattedMessage.FromMarkupOrThrow(Loc.GetString(Alert.Description));
-            return new ActionAlertTooltip(msg, desc) {Cooldown = Cooldown};
+            return new ActionAlertTooltip(msg, desc) {Cooldown = Cooldown, DynamicMessage = DynamicMessage};
         }
 
         /// <summary>

0c846dab3ec9d96c6010f4449915cc842085c3fa
mkanke-real <mikekanke@gmail.com>
2024-12-21 11:10:13 +0900

    Removed brigmed from prototype until its implemented. Added map to testing

--- Content.Benchmarks/MapLoadBenchmark.cs
+++ Content.Benchmarks/MapLoadBenchmark.cs
@@ -46,7 +46,7 @@ public class MapLoadBenchmark
         PoolManager.Shutdown();
     }
 
-    public static readonly string[] MapsSource = { "Empty", "Satlern", "Box", "Bagel", "Dev", "CentComm", "Atlas", "Core", "TestTeg", "Packed", "Origin", "Omega", "Cluster", "Reach", "Meta", "Marathon", "Europa", "MeteorArena", "Fland", "Oasis", "Cog", "FlandHighPop", "OasisHighPop", "OriginHighPop", "Barratry", "Kettle", "Divide", "Glacier" }; //Goobstation, readds maps
+    public static readonly string[] MapsSource = { "Empty", "Satlern", "Box", "Bagel", "Dev", "CentComm", "Atlas", "Core", "TestTeg", "Packed", "Origin", "Omega", "Cluster", "Reach", "Meta", "Marathon", "Europa", "MeteorArena", "Fland", "Oasis", "Cog", "FlandHighPop", "OasisHighPop", "OriginHighPop", "Barratry", "Kettle", "Divide", "Glacier", "roid_outpost" }; //Goobstation, readds maps
 
     [ParamsSource(nameof(MapsSource))]
     public string Map;

768ead67b82a86af4df4aa7f0f1b78d6f5082aa9
marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
2024-12-30 00:32:05 -0500

    add gas abbreviations (#241)
    Co-authored-by: Steve <marlumpy@gmail.com>

--- Content.Shared/Atmos/Atmospherics.cs
+++ Content.Shared/Atmos/Atmospherics.cs
@@ -159,6 +159,10 @@ namespace Content.Shared.Atmos
             [Gas.Plasma] = Loc.GetString("gas-plasma-abbreviation"),
             [Gas.Tritium] = Loc.GetString("gas-tritium-abbreviation"),
             [Gas.WaterVapor] = Loc.GetString("gas-water-vapor-abbreviation"),
+            [Gas.BZ] = Loc.GetString("gas-bz-abbreviation"),
+            [Gas.Healium] = Loc.GetString("gas-healium-abbreviation"),
+            [Gas.Nitrium] = Loc.GetString("gas-nitrium-abbreviation"),
+            [Gas.Pluoxium] = Loc.GetString("gas-pluoxium-abbreviation"),
         };
 
         #region Excited Groups

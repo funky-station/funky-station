3a336ffcd2121952a00cdeeb127958c80d04dcf6
Tadeo <td12233a@gmail.com>
2025-01-10 22:02:03 -0600

    MAKE THIEVES GREAT AGAIN (#280)

--- Content.Shared/Strip/Components/ThievingComponent.cs
+++ Content.Shared/Strip/Components/ThievingComponent.cs
@@ -1,3 +1,6 @@
+using Content.Shared.Alert;
+using Robust.Shared.Prototypes;
+
 namespace Content.Shared.Strip.Components;
 
 /// <summary>
@@ -14,9 +17,20 @@ public sealed partial class ThievingComponent : Component
     public TimeSpan StripTimeReduction = TimeSpan.FromSeconds(0.5f);
 
     /// <summary>
+    /// The preset stripTimeReduction
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan DefaultTimeReduction; // funkystation
+
+    /// <summary>
     /// Should it notify the user if they're stripping a pocket?
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("stealthy")]
     public bool Stealthy;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public ProtoId<AlertPrototype> ThievingAlertProtoId = "Thieving";
 }
+
+public sealed partial class ThievingToggleEvent : BaseAlertEvent; // funkystation
--- Content.Shared/Strip/ThievingSystem.cs
+++ Content.Shared/Strip/ThievingSystem.cs
@@ -1,18 +1,58 @@
+using Content.Shared.Alert;
 using Content.Shared.Inventory;
-using Content.Shared.Strip;
+using Content.Shared.Mindshield.Components;
 using Content.Shared.Strip.Components;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Strip;
 
 public sealed class ThievingSystem : EntitySystem
 {
+    [Dependency] private readonly AlertsSystem _alertsSystem = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<ThievingComponent, BeforeStripEvent>(OnBeforeStrip);
+        SubscribeLocalEvent<ThievingComponent, ComponentInit>(OnCompInit);
+        SubscribeLocalEvent<ThievingComponent, ComponentRemove>(OnCompRemove);
         SubscribeLocalEvent<ThievingComponent, InventoryRelayedEvent<BeforeStripEvent>>((e, c, ev) => OnBeforeStrip(e, c, ev.Args));
+        SubscribeLocalEvent<ThievingComponent, ThievingToggleEvent>(OnThievingToggle);
+    }
+
+    private void OnCompInit(EntityUid uid, ThievingComponent comp, ComponentInit args)
+    {
+        comp.DefaultTimeReduction = comp.StripTimeReduction;
+
+        _alertsSystem.ShowAlert(uid, comp.ThievingAlertProtoId, 2);
+    }
+
+    private void OnCompRemove(EntityUid uid, ThievingComponent comp, ComponentRemove args)
+    {
+        _alertsSystem.ClearAlert(uid, comp.ThievingAlertProtoId);
+    }
+
+    private void OnThievingToggle(Entity<ThievingComponent> ent, ref ThievingToggleEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        ent.Comp.Stealthy = !ent.Comp.Stealthy;
+        ent.Comp.StripTimeReduction = ent.Comp.Stealthy ? ent.Comp.DefaultTimeReduction : TimeSpan.Zero;
+
+        switch (ent.Comp.Stealthy)
+        {
+            case false:
+                _alertsSystem.ShowAlert(ent.Owner, ent.Comp.ThievingAlertProtoId, 1);
+                break;
+
+            case true:
+                _alertsSystem.ShowAlert(ent.Owner, ent.Comp.ThievingAlertProtoId, 2);
+                break;
+        }
+
+        args.Handled = true;
     }
 
     private void OnBeforeStrip(EntityUid uid, ThievingComponent component, BeforeStripEvent args)

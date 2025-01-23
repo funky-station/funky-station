4bcb94cfb081ae7577a5fc17837c37acf8e36712
empty0set <16693552+empty0set@users.noreply.github.com>
2025-01-22 18:35:23 -0700

    Deal with case-sensitivity
    Bruh I don't know C#.
    Anways, you can now have various flavors of 'funk', 'Funk', 'fUNk', or 'FUNK'

--- Content.Server/Speech/EntitySystems/FunkyAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/FunkyAccentSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Content.Server.Speech.Components;
 using Robust.Shared.Random;
 
@@ -17,10 +18,30 @@ namespace Content.Server.Speech.EntitySystems
 
         public string Accentuate(string message)
         {
+            var caseBits = new BitArray(message.Length);
+            var index = 0;
+            foreach (var letter in message)
+            {
+                if (char.IsUpper(letter))
+                    caseBits[index] = true;
+
+                index++;
+            }
+
             foreach (var (word, repl) in SpecialWords)
             {
-                message = message.Replace(word, repl);
+                message = message.ToLower().Replace(word, repl);
             }
+
+            var array = message.ToCharArray();
+            for (int i = 0; i < caseBits.Length; i++)
+            {
+                if (caseBits[i])
+                {
+                    array[i] = char.ToUpper(array[i]);
+                }
+            }
+            message = new string(array);
 
             return message;
         }

2648ab101e06f4e9b495798348f3f165ac2f933d
Kyle Tyo <akikai297@gmail.com>
2025-02-12 02:59:46 -0500

    only you can prevent oopsie daisies.

--- Content.Server/Atmos/Piping/Unary/EntitySystems/GasVentPumpSystem.cs
+++ Content.Server/Atmos/Piping/Unary/EntitySystems/GasVentPumpSystem.cs
@@ -41,6 +41,7 @@ using Content.Server.DeviceNetwork.Systems;
 using Content.Server.NodeContainer.EntitySystems;
 using Content.Server.NodeContainer.Nodes;
 using Content.Server.Power.Components;
+using Content.Shared.Administration.Logs;
 using Content.Shared.Atmos;
 using Content.Shared.Atmos.Monitor;
 using Content.Shared.Atmos.Piping.Components;

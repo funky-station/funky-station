573621bd4e48c182c1536b7d00116c5ef7697a94
SaffronFennec <firefoxwolf2020@protonmail.com>
2025-02-20 01:20:11 +0000

    Update ReligionSystem.cs

--- Content.Server/_Goobstation/Religion/ReligionSystem.cs
+++ Content.Server/_Goobstation/Religion/ReligionSystem.cs
@@ -18,21 +18,21 @@ public sealed class ReligionSystem: EntitySystem
     }
 
     private void OnSpawnComplete(PlayerSpawnCompleteEvent args)
-    //{
-    //
-    //    if (HasComp<BibleUserComponent>(args.Mob)) //Theoretically this can be used to let everyone spawn with the bible of their chosen faith
-    //    {
-    //        if (EntityManager.TryGetComponent(args.Mob, out ReligionComponent? mobReligion))
-    //        {
-   //             var bible = mobReligion.Type switch
-   //             {
-   //                 Shared._Goobstation.Religion.Religion.Atheist => "BibleAtheist",
-   //                 Shared._Goobstation.Religion.Religion.Buddhist => "BibleBuddhist",
-   //                 Shared._Goobstation.Religion.Religion.Christian => "Bible",
-   //                 Shared._Goobstation.Religion.Religion.None => "Bible",
-   //             };
-   //             _inventorySystem.SpawnItemInSlot(args.Mob, "pocket1", bible, true, true);
-   //         }
-   //     }
-  //  }
+    {
+    
+        if (HasComp<BibleUserComponent>(args.Mob)) //Theoretically this can be used to let everyone spawn with the bible of their chosen faith
+        {
+            if (EntityManager.TryGetComponent(args.Mob, out ReligionComponent? mobReligion))
+            {
+                var bible = mobReligion.Type switch
+                {
+                    Shared._Goobstation.Religion.Religion.Atheist => "BibleAtheist",
+                    Shared._Goobstation.Religion.Religion.Buddhist => "BibleBuddhist",
+                    Shared._Goobstation.Religion.Religion.Christian => "Bible",
+                    Shared._Goobstation.Religion.Religion.None => "Bible",
+                };
+                _inventorySystem.SpawnItemInSlot(args.Mob, "pocket1", bible, false, false);
+            }
+        }
+    }
 }

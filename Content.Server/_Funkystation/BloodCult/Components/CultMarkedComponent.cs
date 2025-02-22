81c53ccbc1d9a0605f2d22c4924e9074f8deacf0
Kevin Barkevich <kbarkevich@gmail.com>
2025-02-22 05:18:53 -0500

    Added Stun spell

---
+++ Content.Server/_Funkystation/BloodCult/Components/CultMarkedComponent.cs
@@ -0,0 +1,9 @@
+using Content.Server.GameTicking.Rules;
+
+namespace Content.Server.BloodCult.Components;
+
+/// <summary>
+/// Given to people marked by the Blood Cult spell Stun.
+/// </summary>
+[RegisterComponent]
+public sealed partial class CultMarkedComponent : Component;
--- Content.Server/_Funkystation/BloodCult/EntitySystems/CultistSpellSystem.cs
+++ Content.Server/_Funkystation/BloodCult/EntitySystems/CultistSpellSystem.cs
@@ -1,15 +1,33 @@
 using Robust.Shared.Random;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Timing;
+using Content.Server.Power.Components;
 using Content.Shared.FixedPoint;
 using Content.Server.Popups;
 using Content.Server.Hands.Systems;
 using Content.Shared.Actions;
 using Content.Shared.BloodCult;
 using Content.Shared.BloodCult.Prototypes;
+using Content.Server.BloodCult.Components;
+using Content.Shared.BloodCult.Components;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Prototypes;
+using Content.Shared.Damage.Systems;
 using Content.Server.GameTicking.Rules;
+using Content.Shared.StatusEffect;
+using Content.Shared.Speech.Muting;
+using Content.Shared.Stunnable;
+using Content.Shared.Emp;
+using Content.Server.Emp;
+using Content.Server.PowerCell;
+using Content.Shared.PowerCell;
+using Content.Shared.PowerCell.Components;
+using Content.Shared.Silicons.Borgs.Components;
+using Content.Shared.Weapons.Melee;
+using Content.Shared.Weapons.Melee.Events;
+
 
 namespace Content.Server.BloodCult.EntitySystems;
 
@@ -21,8 +39,16 @@ public sealed partial class CultistSpellSystem : EntitySystem
 	[Dependency] private readonly DamageableSystem _damageableSystem = default!;
 	[Dependency] private readonly PopupSystem _popup = default!;
 	[Dependency] private readonly SharedAudioSystem _audioSystem = default!;
+	[Dependency] private readonly StatusEffectsSystem _statusEffect = default!;
 	[Dependency] private readonly BloodCultRuleSystem _bloodCultRules = default!;
 	[Dependency] private readonly HandsSystem _hands = default!;
+	[Dependency] private readonly StaminaSystem _stamina = default!;
+	[Dependency] private readonly EmpSystem _emp = default!;
+	[Dependency] private readonly PowerCellSystem _powerCell = default!;
+	[Dependency] private readonly IGameTiming _gameTiming = default!;
+	[Dependency] private readonly IEntityManager _entMan = default!;
+	[Dependency] private readonly SharedStunSystem _stun = default!;
+	
 	[Dependency] private readonly IPrototypeManager _protoMan = default!;
 
 	private static string[] AvailableDaggers = ["CultDaggerCurved", "CultDaggerSerrated", "CultDaggerStraight"];
@@ -32,6 +58,9 @@ public sealed partial class CultistSpellSystem : EntitySystem
 		base.Initialize();
 
 		SubscribeLocalEvent<BloodCultistComponent, EventCultistSummonDagger>(OnSummonDagger);
+
+		SubscribeLocalEvent<BloodCultistComponent, EventCultistStun>(OnStun);
+		SubscribeLocalEvent<CultMarkedComponent, AttackedEvent>(OnMarkedAttacked);
 	}
 
 	private bool TryUseAbility(Entity<BloodCultistComponent> ent, BaseActionEvent args)
@@ -111,5 +140,51 @@ public sealed partial class CultistSpellSystem : EntitySystem
 		}
 
 		args.Handled = true;
+	}
+
+	private void OnStun(Entity<BloodCultistComponent> ent, ref EventCultistStun args)
+	{
+		if (args.Handled || !TryUseAbility(ent, args) || HasComp<BloodCultistComponent>(args.Target))
+			return;
+
+		float staminaDamage = 90f;
+		float empDamage = 1000f;
+		int stunTime = 10;
+
+		args.Handled = true;
+
+		var target = args.Target;
+
+		if (HasComp<BorgChassisComponent>(target) &&
+			_powerCell.TryGetBatteryFromSlot(target, out EntityUid? batteryUid, out BatteryComponent? _) &&
+			batteryUid != null)
+		{
+			_emp.DoEmpEffects((EntityUid)batteryUid, empDamage, stunTime);
+		}
+		else
+		{
+			_stun.TryKnockdown(target, TimeSpan.FromSeconds(stunTime), true);
+			_stamina.TakeStaminaDamage(target, staminaDamage, visual: false);
+			EnsureComp<CultMarkedComponent>(target);
+		}
+
+		_statusEffect.TryAddStatusEffect<MutedComponent>(target, "Muted", TimeSpan.FromSeconds(stunTime), false);
+
+		args.Handled = true;
+	}
+
+	private void OnMarkedAttacked(Entity<CultMarkedComponent> ent, ref AttackedEvent args)
+	{
+		var advancedStaminaDamage = 100;
+		var advancedStunTime = 15;
+		if (HasComp<BloodCultRuneCarverComponent>(args.Used))
+		{
+			_stun.TryKnockdown(ent, TimeSpan.FromSeconds(advancedStunTime), true);
+			_stamina.TakeStaminaDamage(ent, advancedStaminaDamage, visual: false);
+			_stun.TryStun(ent, TimeSpan.FromSeconds(advancedStunTime), true);
+			_statusEffect.TryAddStatusEffect<MutedComponent>(ent, "Muted", TimeSpan.FromSeconds(advancedStunTime), false);
+			_entMan.RemoveComponent<CultMarkedComponent>(ent);
+			_audioSystem.PlayPvs(new SoundPathSpecifier("/Audio/Items/Defib/defib_zap.ogg"), ent, AudioParams.Default.WithVolume(-3f));
+		}
 	}
 }

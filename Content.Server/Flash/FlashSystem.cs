ef69ee188b162b7827580f803b3517559b01c4fb
taydeo <td12233a@gmail.com>
2025-02-23 19:40:41 -0600

    fix: revs can flash people again

--- Content.Server/Flash/FlashSystem.cs
+++ Content.Server/Flash/FlashSystem.cs
@@ -146,13 +146,14 @@ namespace Content.Server.Flash
 
             if (melee || revFlash)
             {
-                var ev = new AfterFlashedEvent(target, user, used);
-
                 if (user == null)
                     return;
 
                 if (used == null)
                     return;
+
+                var ev = new AfterFlashedEvent(target, user, used);
+                RaiseLocalEvent(target, ref ev);
 
                 _popup.PopupEntity(Loc.GetString("flash-component-user-head-rev",
                         ("victim", Identity.Entity(target, EntityManager))), target);

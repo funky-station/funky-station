785aa072d77266343ca6922bc9a8dab9ee8b6164
kbarkevich <24629810+kbarkevich@users.noreply.github.com>
2025-03-05 15:49:09 -0500

    Bloodcult rebalance this time with guidebook (#487)
    * Added cult knife that can carve and erase runes
    
    * Added ability to carve runes using a cult knife
    
    * Added Barrier Rune
    
    * Created assignable Blood Cultist antag role
    
    * Added cultist job icon and starting sound effect
    
    * Added Revive Rune
    
    * Added verbal invocations
    
    * Added Offer Rune
    
    * Added cult targets (not displayed yet) and support for sacrifficng heads or brains
    
    * Added logic to stop cultist target selector from picking a cultist
    
    * Added cult spell prototypes and Summon Dagger spell
    
    * Added Stun spell
    
    * Added Study Veil action
    
    * Added weak veil announcement and moved announcements to language file
    
    * Added Commune action to cultists
    
    * Made conversions count towards revive rune charges
    
    * Added spell selection ability, the last of the blood cult starting abilities
    
    * Completed cult spell preparation logic and added Empowering Rune
    
    * Added incantation to empowering rune
    
    * Fixed bug where runes being next to opening doors would crash the server
    
    * Added tear veil rune and Cult victory
    
    * Added cult structure entities with animations, and implemented functionality for the Pylon structure.
    
    * Stop non-cultists from using cult stuff
    
    * Moved bare string into localization file
    
    * Added veil tear location selection
    
    * Updated language file
    
    * Added runed metal
    
    * Made cult pylon constructable and gave cultists 10 runed metal to start
    
    * Added Juggernaut constructable entity, fueled by Soul Stones when players are sacrificed on offer runes
    
    * Added Twisted Construction spell
    
    * Added logic for final summoning rune placement.
    
    * Added ability to de-convert cultists with holy water
    
    * Changed rune drawing animation to an effect and changed rune color
    
    * fixed lint error
    
    * reverted debug-era nar'sie summon condition
    
    * fixed serialization error
    
    * Applied Tay fixes
    
    * Fixed issue where IPCs could not use basic cult spell menus
    
    * Fixed communes not being logged or visible to ghosts
    
    * Fixed various cult UI bugs (especially commune)
    
    * Marked cult items as contraband
    
    * fixed ghosts seeing all cult announcements
    
    * Increased invocation range for offer runes
    
    * fixed commune menu keyboard focus bug and fixed issue where cultists would not show up as antagonists in the ahelp menu
    
    * Fixed issue where cult target cryoing or going catatonic would stall the round
    
    * Fixed issue where harpies could not use cult menus
    
    * Added halo giveaway when cultists reach 30% membership
    
    * Immediately rise cultists if the final target was sacrificed
    
    * Loosened restrictions on tear veil rune, making only 6 tiles need to have a cultist on it
    
    * Fixed issue where ghosts could not hear nar'sie being summoned
    
    * Made chaplains resistant to conversion and sacrifice. Chaplains also cannot be selected as targets.
    
    * Deconverted cultists now lose their halos
    
    * Weakened juggernaut's HP a bit
    
    * Added new construct, the Shade, which can be summoned by activating a soul stone in one's hand.
    
    * Stopped dead Shades from being able to be recalled
    
    * Added blood cult guidebook entry
    
    * Slight revisement of the blood cult guidebook
    
    * Added glowey eyes
    
    * Allow reversal of loyalties with cult deconversion

---
+++ Content.Shared/_Funkystation/BloodCult/Components/BloodCultConstructShellComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.BloodCult.Components;
+
+/// <summary>
+/// A hollow shell awaiting a soul
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class BloodCultConstructShellComponent : Component
+{
+}

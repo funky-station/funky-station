5cd4453a96d4ea2f1081fbe58e3676e0ccfec231
DevilishMilk <michaellapjr@gmail.com>
2025-03-11 20:10:59 -0400

    minor spelling mistake

--- Content.Server/Flash/FlashSystem.cs
+++ Content.Server/Flash/FlashSystem.cs
@@ -140,7 +140,7 @@ namespace Content.Server.Flash
 
             if (displayPopup && user != null && target != user && Exists(user.Value))
             {
-                _popup.PopupEntity(Loc.GetString("flash-component-user-bAlinds-you",
+                _popup.PopupEntity(Loc.GetString("flash-component-user-blinds-you",
                     ("user", Identity.Entity(user.Value, EntityManager))), target, target);
             }
 

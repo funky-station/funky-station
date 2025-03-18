1eea49eb4b26f6a2e0a6be8636ecbab385b5a54e
corresp0nd <46357632+corresp0nd@users.noreply.github.com>
2025-03-18 10:38:51 -0700

    pr 2004

--- Content.Server/_Impstation/CosmicCult/CosmicCultRuleSystem.cs
+++ Content.Server/_Impstation/CosmicCult/CosmicCultRuleSystem.cs
@@ -187,6 +187,7 @@ public sealed class CosmicCultRuleSystem : GameRuleSystem<CosmicCultRuleComponen
             var winner = Identity.Entity(picked, EntityManager);
             _adminLogger.Add(LogType.Vote, LogImpact.Medium, $"Cult stewardship vote finished: {winner} is now steward.");
             AddComp<CosmicCultLeadComponent>(picked);
+            _antag.SendBriefing(picked, Loc.GetString("cosmiccult-vote-leadership-briefing"), Color.FromHex("#4cabb3"), _monumentAlert);
         };
     }
 
@@ -856,5 +857,3 @@ public sealed class CosmicCultRuleSystem : GameRuleSystem<CosmicCultRuleComponen
     }
     #endregion
 }
-
-

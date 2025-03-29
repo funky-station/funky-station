b5d5148d710601ff52999ff7b5841237c0ff9bae
aa5g21 <aa5g21@soton.ac.uk>
2025-03-29 02:26:46 +0000

    Removed copied comments

--- Content.Shared/Cargo/Prototypes/CargoBountyCategoryPrototype.cs
+++ Content.Shared/Cargo/Prototypes/CargoBountyCategoryPrototype.cs
@@ -3,24 +3,15 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared.Cargo.Prototypes;
 
-/// <summary>
-/// This is a prototype for a cargo bounty, a set of items
-/// that must be sold together in a labeled container in order
-/// to receive a monetary reward.
-/// </summary>
 [Prototype, Serializable, NetSerializable]
 public sealed partial class CargoBountyCategoryPrototype : IPrototype
 {
-    /// <inheritdoc/>
     [IdDataField]
     public string ID { get; private set; } = default!;
 
     [DataField(required: true)]
     public string Name { get; private set; } = default!;
 
-    /// <summary>
-    /// Holds the category information for prototypes
-    /// </summary>
     [DataField(required: true)]
     public required List<CargoBountyItemEntry> Entries;
 }

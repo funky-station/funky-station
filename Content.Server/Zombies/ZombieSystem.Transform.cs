6fa4ce90327cd981e3aa73f13a0ccfc3d5dbca24
J <billsmith116@gmail.com>
2025-04-05 00:20:19 +0000

    Extracts magic strings from Tag calls (#36305)
    * Extracts magic strings from Tag calls
    
    When #36281 gets merged, the `TagSystem` methods will all give warnings. Let's fix those warnings before they even happen!
    
    * Adds missing libraries
    
    * Remove not yet implemented TagSystem changes
    
    * Fix tag spelling error
    
    Genuinely surprised there was only 1!
    
    * Styling and proper type changes
    
    * Styling
    
    Co-authored-by: Tayrtahn <tayrtahn@gmail.com>
    
    ---------
    
    Co-authored-by: Tayrtahn <tayrtahn@gmail.com>

--- Content.Server/Zombies/ZombieSystem.Transform.cs
+++ Content.Server/Zombies/ZombieSystem.Transform.cs
@@ -37,6 +37,7 @@ using Robust.Shared.Audio.Systems;
 using Content.Shared.Ghost.Roles.Components;
 using Content.Shared.NPC.Components;
 using Content.Shared.Tag;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server.Zombies;
 
@@ -61,6 +62,8 @@ public sealed partial class ZombieSystem
     [Dependency] private readonly NPCSystem _npc = default!;
     [Dependency] private readonly SharedRoleSystem _roles = default!;
     [Dependency] private readonly TagSystem _tag = default!;
+
+    private static readonly ProtoId<TagPrototype> InvalidForGlobalSpawnSpellTag = "InvalidForGlobalSpawnSpell";
 
     /// <summary>
     /// Handles an entity turning into a zombie when they die or go into crit
@@ -292,6 +295,6 @@ public sealed partial class ZombieSystem
 
         //Need to prevent them from getting an item, they have no hands.
         // Also prevents them from becoming a Survivor. They're undead.
-        _tag.AddTag(target, "InvalidForGlobalSpawnSpell");
+        _tag.AddTag(target, InvalidForGlobalSpawnSpellTag);
     }
 }

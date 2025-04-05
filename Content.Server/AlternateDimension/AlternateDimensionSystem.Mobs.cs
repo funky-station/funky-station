1512076e2d7e51018fcecfde3f5e04608cb18bec
empty0set <16693552+empty0set@users.noreply.github.com>
2025-04-05 16:44:58 -0600

    Initial de-coupling of entrance / exit portals
    - This needs tweaking / not literally copy + pasting code
    -- General idea is that the portal out of the shadow lands is different from the one that puts you there.  This, psychologically, is very different from before as you no longer have a safe place to retreat to, and instead are basically forced to explore more
    --- One problem is that since the exit portal is spawned randomly we don't really have a good way of leading the player there, through breadcrumbs, exploration, what-have-you.  This will absolutely be a problem on larger maps.  Will have to brainstorm this more.

--- Content.Server/AlternateDimension/AlternateDimensionSystem.Mobs.cs
+++ Content.Server/AlternateDimension/AlternateDimensionSystem.Mobs.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using Content.Server.Antag;
 using Content.Server.Ghost.Roles.Components;
 using Content.Shared.Physics;
+using Content.Shared.Teleportation.Components;
 using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
@@ -38,8 +39,8 @@ public sealed partial class AlternateDimensionSystem
             return;
         if (!EntityManager.TryGetComponent<MapGridComponent>(alternateGridId, out var alternateGridComp))
             return;
-
-        sawmill.Log(LogLevel.Warning, "Wowee!");
+        if (!EntityManager.TryGetComponent<MapGridComponent>(originalGridId, out var originalGridComp))
+            return;
 
         // radius is based off the of the size of the map & can be adjusted in the .yml to taste
         var circleRadius = alternateGridComp.LocalAABB.Width * ent.Comp.RadiusModifier * 0.5f;
@@ -48,8 +49,11 @@ public sealed partial class AlternateDimensionSystem
         var alternateTiles = new List<(Vector2i Index, Tile Tile)>();
 
         var tilesRemoved = 0;
+        var backupTile = new ValueTuple<Vector2i, Tile>(Vector2i.Zero, Tile.Empty);
         while (stationTiles.MoveNext(out var tileRef))
         {
+            backupTile = (tileRef.Value.GridIndices, tileRef.Value.Tile); // in case this is configured wrong we need at least one guaranteed place to spawn
+                                                                          // the portal back
             var circle = new Circle(coords.Value.Position, circleRadius);
             var tileBox = new Box2(tileRef.Value.GridIndices * alternateGridComp.TileSize,
                 (tileRef.Value.GridIndices + Vector2i.One) * alternateGridComp.TileSize);
@@ -62,7 +66,36 @@ public sealed partial class AlternateDimensionSystem
             alternateTiles.Add((tileRef.Value.GridIndices, tileRef.Value.Tile));
         }
 
-        sawmill.Log(LogLevel.Debug, "Removed {0} tiles from mob spawn consideration.", tilesRemoved);
+        sawmill.Log(LogLevel.Debug, "Removed {0} tiles from mob spawn consideration around ({1},{2}) with radius = {3}",
+            tilesRemoved,
+            coords.Value.Position.X,
+            coords.Value.Position.Y,
+            circleRadius);
+
+        if (TryComp<AlternateDimensionAutoPortalComponent>(ent, out var AutoPortalComp))
+        {
+            var portalTile = backupTile;
+            if (alternateTiles.Count > 0)
+            {
+                portalTile = alternateTiles.RemoveSwap(_random.Next(alternateTiles.Count));
+            }
+
+            var otherEnt = SpawnAtPosition(AutoPortalComp.OtherSidePortal,
+                originalGridComp.GridTileToLocal(portalTile.Item1));
+            var otherOtherEnt = SpawnAtPosition("PortalBlue",
+                alternateGridComp.GridTileToLocal(portalTile.Item1));
+            _link.TryLink(otherEnt, otherOtherEnt, true);
+
+            //Make sure
+            if (TryComp<PortalComponent>(otherEnt, out var portal1))
+            {
+                portal1.CanTeleportToOtherMaps = true;
+            }
+            if (TryComp<PortalComponent>(otherOtherEnt, out var portal2))
+            {
+                portal2.CanTeleportToOtherMaps = true;
+            }
+        }
 
         var players = _antag.GetTotalPlayerCount(_player.Sessions);
         var count = Math.Clamp(Convert.ToInt32(players * ent.Comp.PlayerScaling), ent.Comp.Min, ent.Comp.Max);
@@ -96,6 +129,5 @@ public sealed partial class AlternateDimensionSystem
                 coords.Value.Position.X,
                 coords.Value.Position.Y);
         }
-
     }
 }

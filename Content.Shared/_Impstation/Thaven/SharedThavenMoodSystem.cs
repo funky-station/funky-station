d0b8eb38f3b658a3bfd244aaa2b4763d1891904d
corresp0nd <46357632+corresp0nd@users.noreply.github.com>
2025-04-08 20:20:45 -0700

    report DV version, backend cleanup

--- Content.Shared/_Impstation/Thaven/SharedThavenMoodSystem.cs
+++ Content.Shared/_Impstation/Thaven/SharedThavenMoodSystem.cs
@@ -5,25 +5,14 @@ namespace Content.Shared._Impstation.Thaven;
 
 public abstract class SharedThavenMoodSystem : EntitySystem
 {
-    [Dependency] private readonly EmagSystem _emag = default!;
-
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<ThavenMoodsComponent, GotEmaggedEvent>(OnEmagged);
+
+        // SubscribeLocalEvent<ThavenMoodsComponent, GotEmaggedEvent>(OnEmagged); DeltaV: no emagging thavens
     }
-
-    protected virtual void OnEmagged(Entity<ThavenMoodsComponent> ent, ref GotEmaggedEvent args)
+    protected virtual void OnEmagged(EntityUid uid, ThavenMoodsBoundComponent comp, ref GotEmaggedEvent args)
     {
-        if (!_emag.CompareFlag(args.Type, EmagType.Interaction))
-            return;
-
-        if (_emag.CheckFlag(ent, EmagType.Interaction))
-            return;
-
-        if (ent.Owner == args.UserUid)
-            return;
-
         args.Handled = true;
     }
 }

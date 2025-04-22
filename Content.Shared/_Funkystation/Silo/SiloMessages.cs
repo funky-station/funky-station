9f7f1dcbf40c2d655469d9e32008cd15e003ac7f
aa5g21 <aa5g21@soton.ac.uk>
2025-04-22 13:35:39 +0100

    Changed my mind, ui added

---
+++ Content.Shared/_Funkystation/Silo/SiloMessages.cs
@@ -0,0 +1,24 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._Funkystation.Silo;
+
+[Serializable, NetSerializable]
+public sealed class SiloUpdateState : BoundUserInterfaceState
+{
+
+}
+
+/// <summary>
+///     Sent to the server to sync material storage and the recipe queue.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class LatheSyncRequestMessage : BoundUserInterfaceMessage
+{
+
+}
+
+[NetSerializable, Serializable]
+public enum SiloUiKey
+{
+    Key,
+}

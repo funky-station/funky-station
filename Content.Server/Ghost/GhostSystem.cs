7cb220e788dbf913a34576ba415d539ca61d3efe
keronshb <54602815+keronshb@users.noreply.github.com>
2025-04-28 13:42:24 -0400

    Wizard Teleport Scroll (Teleport Location ECS) (#36424)

--- Content.Server/Ghost/GhostSystem.cs
+++ Content.Server/Ghost/GhostSystem.cs
@@ -67,6 +67,7 @@ using Content.Shared.NameModifier.EntitySystems;
 using Content.Shared.Popups;
 using Content.Shared.Storage.Components;
 using Content.Shared.Tag;
+using Content.Shared.Warps;
 using Robust.Server.GameObjects;
 using Robust.Shared.Configuration;
 using Robust.Shared.Map;

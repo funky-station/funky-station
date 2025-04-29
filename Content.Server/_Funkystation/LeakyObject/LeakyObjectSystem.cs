ef906a97990f94cd42cc37c01a54eb2174e2b595
Sophia Rustfield <gitlab@catwolf.xyz>
2025-04-29 11:20:20 +0200

    make insuls immune to leaky plushies

--- Content.Server/_Funkystation/LeakyObject/LeakyObjectSystem.cs
+++ Content.Server/_Funkystation/LeakyObject/LeakyObjectSystem.cs
@@ -3,6 +3,8 @@ using Content.Server.Body.Components;
 using Content.Server.Body.Systems;
 using Content.Shared.Chemistry;
 using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Electrocution;
+using Content.Shared.Inventory;
 using Robust.Shared.Containers;
 using Robust.Shared.Timing;
 
@@ -14,6 +16,7 @@ public sealed class LeakyObjectSystem : EntitySystem
     [Dependency] private readonly ILogManager _logManager = default!;
     [Dependency] private readonly ReactiveSystem _reactiveSystem = default!;
     [Dependency] private readonly BloodstreamSystem _bloodstreamSystem = default!;
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
     private ISawmill _sawmill = default!;
 
     public override void Initialize()
@@ -41,6 +44,15 @@ public sealed class LeakyObjectSystem : EntitySystem
             if (!_solutionContainers.TryGetSolution(uid, comp.SolutionName, out var soln, out var solution) ||
                 !_containers.TryGetContainingContainer((uid, null, null), out var container) ||
                 !TryComp(container.Owner, out BloodstreamComponent? bloodstream))
+            {
+                continue;
+            }
+
+            // i don't actually like that this makes insuls immune, i would prefer it if the janitorial gloves were immune instead, but they don't have a special component
+            // TODO: give jani gloves a component like that and use it here instead.
+            if (_inventorySystem.TryGetSlotEntity(container.Owner, "gloves", out var glovesUid) &&
+                EntityManager.HasComponent<InsulatedComponent>(glovesUid) &&
+                !(_inventorySystem.TryGetSlotEntity(container.Owner, "head", out var headUid) && headUid == uid))
             {
                 continue;
             }

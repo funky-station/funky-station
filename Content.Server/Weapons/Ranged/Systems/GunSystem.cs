29457f95ee9987ac1d905a19a5795629bf36b109
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-05-06 09:39:05 -0700

    StaminaSystem to SharedStaminaSystem (#37199)
    * Init Commit
    
    * Partial class
    
    * Hands system slipped through

--- Content.Server/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.cs
@@ -32,7 +32,7 @@ public sealed partial class GunSystem : SharedGunSystem
     [Dependency] private readonly DamageExamineSystem _damageExamine = default!;
     [Dependency] private readonly PricingSystem _pricing = default!;
     [Dependency] private readonly SharedColorFlashEffectSystem _color = default!;
-    [Dependency] private readonly StaminaSystem _stamina = default!;
+    [Dependency] private readonly SharedStaminaSystem _stamina = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly SharedMapSystem _map = default!;
 

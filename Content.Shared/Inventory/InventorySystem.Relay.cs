b643ea8298c8e8584725a432c61c1c567a6be6dd
Winkarst <74284083+Winkarst-cpu@users.noreply.github.com>
2025-05-12 20:35:42 +0300

    New Feature: Slot blockers (#35172)
    * First commit
    
    * More comments
    
    * Update
    
    * Update
    
    * For Beloved Maintainers
    
    * Beck T, my beloved
    
    * Update
    
    * Old stuff
    
    * Update EquipAttemptEvents.cs
    
    * Update UnequipAttemptEvent.cs
    
    ---------
    
    Co-authored-by: beck-thompson <107373427+beck-thompson@users.noreply.github.com>

--- Content.Shared/Inventory/InventorySystem.Relay.cs
+++ Content.Shared/Inventory/InventorySystem.Relay.cs
@@ -50,6 +50,8 @@ public partial class InventorySystem
         SubscribeLocalEvent<InventoryComponent, CoefficientQueryEvent>(RelayInventoryEvent);
         SubscribeLocalEvent<InventoryComponent, CheckMagicItemEvent>(RelayInventoryEvent); //heretic FunkyStation
         SubscribeLocalEvent<InventoryComponent, ZombificationResistanceQueryEvent>(RelayInventoryEvent);
+        SubscribeLocalEvent<InventoryComponent, IsEquippingTargetAttemptEvent>(RelayInventoryEvent);
+        SubscribeLocalEvent<InventoryComponent, IsUnequippingTargetAttemptEvent>(RelayInventoryEvent);
 
         // by-ref events
         SubscribeLocalEvent<InventoryComponent, RefreshFrictionModifiersEvent>(RefRelayInventoryEvent);

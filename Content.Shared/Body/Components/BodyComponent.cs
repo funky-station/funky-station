d2e404e5be46febde53fc25987ed7743fcd52c6d
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2025-05-12 11:26:34 +0400

    delaggening #2744

--- Content.Shared/Body/Components/BodyComponent.cs
+++ Content.Shared/Body/Components/BodyComponent.cs
@@ -5,8 +5,11 @@
 // SPDX-FileCopyrightText: 2023 Jezithyr <jezithyr@gmail.com>
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 metalgearsloth <comedian_vs_clown@hotmail.com>
-// SPDX-FileCopyrightText: 2025 V <97265903+formlessnameless@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+// SPDX-FileCopyrightText: 2025 Spatison <137375981+Spatison@users.noreply.github.com>
 //
 // SPDX-License-Identifier: MIT
 
@@ -16,6 +19,7 @@ using Robust.Shared.Audio;
 using Robust.Shared.Containers;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
+using System; // Goobstation
 
 namespace Content.Shared.Body.Components;
 
@@ -60,4 +64,12 @@ public sealed partial class BodyComponent : Component
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool ThermalVisibility = true;
+    // WD EDIT END
+
+    // Goobstation
+    /// <summary>
+    /// When should  wounds on this be healed.
+    /// </summary>
+    [ViewVariables, AutoNetworkedField, Access(Other = AccessPermissions.ReadWrite)]
+    public TimeSpan HealAt;
 }

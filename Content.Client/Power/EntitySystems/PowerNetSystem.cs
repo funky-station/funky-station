5658726819ef92e3060ddd0c730bf5b6370eba20
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-05-13 21:49:43 +1000

    Predict gasthermomachines (#33837)
    * Predict gasthermomachines
    
    * despawn
    
    * smellby

--- Content.Client/Power/EntitySystems/PowerNetSystem.cs
+++ Content.Client/Power/EntitySystems/PowerNetSystem.cs
@@ -3,11 +3,22 @@
 //
 // SPDX-License-Identifier: MIT
 
+using Content.Client.Power.Components;
+using Content.Shared.Power.Components;
 using Content.Shared.Power.EntitySystems;
 
 namespace Content.Client.Power.EntitySystems;
 
 public sealed class PowerNetSystem : SharedPowerNetSystem
 {
+    public override bool IsPoweredCalculate(SharedApcPowerReceiverComponent comp)
+    {
+        return IsPoweredCalculate((ApcPowerReceiverComponent)comp);
+    }
 
+    private bool IsPoweredCalculate(ApcPowerReceiverComponent comp)
+    {
+        return !comp.PowerDisabled
+               && !comp.NeedsPower;
+    }
 }

99dfb6b1eb43a272a01d2c403db0bf69e3ee9eea
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2025-05-17 21:11:08 +0400

    Nothing to see here...

--- Content.Shared.Database/LogType.cs
+++ Content.Shared.Database/LogType.cs
@@ -509,5 +509,10 @@ public enum LogType
     /// <summary>
     /// GameDirector related game interactions.
     /// </summary>
-    GameDirector = 102 // Goobstation
+    GameDirector = 102, // Goobstation
+
+    /// <summary>
+    /// Damaging grid collision has occurred.
+    /// </summary>
+    ShuttleImpact = 103
 }

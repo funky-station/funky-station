490ebb5e28794c70bf4e26a0fcf89dd8d5361b98
Ghast <wolfsparkvanb@gmail.com>
2025-05-19 15:14:26 -0500

    added paymentsalary prototype type, tweaks

---
+++ Content.Server/_Funkystation/Payouts/Prototypes/PaymentSalaryPrototype.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Funkystation.Payouts.Prototypes;
+
+[Prototype("paymentSalary")]
+public sealed partial class PaymentSalaryPrototype : IPrototype
+{
+    [IdDataField] public string ID { get; private set; } = default!;
+
+    /// <summary>
+    /// The roles that has this applied to their salary.
+    /// </summary>
+    [DataField("roles")] public List<JobPrototype> Roles = new List<JobPrototype>();
+
+    [DataField("salary")] public int Salary = 0;
+}

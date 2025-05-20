8acd435ffc77ff4830f4eacec456978885968ab2
Tay <td12233a@gmail.com>
2025-05-20 18:45:04 -0500

    Discard changes to Content.Server/Cargo/Components/StationBankAccountComponent.cs

--- Content.Server/Cargo/Components/StationBankAccountComponent.cs
+++ Content.Server/Cargo/Components/StationBankAccountComponent.cs
@@ -1,4 +1,3 @@
-using Content.Server._Funkystation.Payouts;
 using Content.Shared.Cargo;
 
 namespace Content.Server.Cargo.Components;
@@ -6,7 +5,7 @@ namespace Content.Server.Cargo.Components;
 /// <summary>
 /// Added to the abstract representation of a station to track its money.
 /// </summary>
-[RegisterComponent, Access(typeof(SharedCargoSystem), typeof(StationRegularPayoutSystem), typeof(PayoutSystem))]
+[RegisterComponent, Access(typeof(SharedCargoSystem))]
 public sealed partial class StationBankAccountComponent : Component
 {
     [ViewVariables(VVAccess.ReadWrite), DataField("balance")]
@@ -17,25 +16,4 @@ public sealed partial class StationBankAccountComponent : Component
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite), DataField("increasePerSecond")]
     public int IncreasePerSecond = 1;
-
-    /// <summary>
-    /// funky station: company scrip
-    /// starter station scrip balance
-    /// </summary>
-    [ViewVariables(VVAccess.ReadWrite), DataField]
-    public int ScripBalance = 60_000;
-
-    /// <summary>
-    /// funky station: company scrip
-    /// time between payouts
-    /// </summary>
-    [ViewVariables(VVAccess.ReadWrite), DataField]
-    public TimeSpan ScripStationPayoutPeriod = TimeSpan.FromMinutes(10);
-
-    /// <summary>
-    /// funky station: company scrip
-    /// payout people who are detained or wanted
-    /// </summary>
-    [ViewVariables(VVAccess.ReadWrite), DataField]
-    public bool ScripPayoutDetainedOrWanted = false;
 }

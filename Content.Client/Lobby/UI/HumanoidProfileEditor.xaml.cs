3040323f96fe95e96dfe84c4fb0a5e976faeafab
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-05-31 13:16:14 -0400

    Put a short cooldown on the export profile image button. This also serves as basic feedback that the button literally did anything. Also use tasks instead of some private boolean.

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -212,7 +212,7 @@ namespace Content.Client.Lobby.UI
 
             ExportImageButton.OnPressed += args =>
             {
-                Preview.ExportImage();
+                Preview.ExportImage(args.Button);
             };
 
             OpenImagesButton.OnPressed += args =>

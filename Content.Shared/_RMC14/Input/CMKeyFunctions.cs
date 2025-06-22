185fc8d9cf589f1329f888c77127da9cd16916b7
jackel234 <colespayde02@gmail.com>
2025-06-22 14:51:53 -0400

    the kammerer now needs to be pumped in order to fire :godo:

---
+++ Content.Shared/_RMC14/Input/CMKeyFunctions.cs
@@ -0,0 +1,15 @@
+using Robust.Shared.Input;
+
+namespace Content.Shared._RMC14.Input;
+
+[KeyFunctions]
+public sealed class CMKeyFunctions
+{
+
+    public static readonly BoundKeyFunction CMUniqueAction = "CMUniqueAction";
+    public static readonly BoundKeyFunction CMHolsterPrimary = "CMHolsterPrimary";
+    public static readonly BoundKeyFunction CMHolsterSecondary = "CMHolsterSecondary";
+    public static readonly BoundKeyFunction CMHolsterTertiary = "CMHolsterTertiary";
+    public static readonly BoundKeyFunction CMHolsterQuaternary = "CMHolsterQuaternary";
+
+}

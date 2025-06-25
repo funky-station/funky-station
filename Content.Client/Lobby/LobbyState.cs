5990254f91a06147a2f97abe883688ce58c8dfab
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-06-25 19:40:20 -0400

    Update the state of the ready up button after dragging priorities.

--- Content.Client/Lobby/LobbyState.cs
+++ Content.Client/Lobby/LobbyState.cs
@@ -113,6 +113,7 @@ namespace Content.Client.Lobby
             UpdateLobbyUi();
 
             Lobby.CharacterPreview.CharacterSetupButton.OnPressed += OnSetupPressed;
+            Lobby.CharacterPreview.PrioritiesUpdated += UpdateReadyAllowed;
             Lobby.ManifestButton.OnPressed += OnManifestPressed;
             Lobby.ReadyButton.OnPressed += OnReadyPressed;
             Lobby.ReadyButton.OnToggled += OnReadyToggled;

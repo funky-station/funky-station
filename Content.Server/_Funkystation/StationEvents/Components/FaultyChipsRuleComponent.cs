19b1459d396cd43a5ce2d82f057271f2d520f5a8
vectorassembly <vectorassembly@icloud.com>
2025-07-06 11:23:46 +1100

    FaultyChips and revive event

---
+++ Content.Server/_Funkystation/StationEvents/Components/FaultyChipsRuleComponent.cs
@@ -0,0 +1,10 @@
+using Content.Server.StationEvents.Events;
+
+namespace Content.Server.StationEvents.Components;
+
+[RegisterComponent]
+[Access(typeof(FaultyChipsRule))]
+public sealed partial class FaultyChipsRuleComponent : Component
+{
+
+}
---
+++ Content.Server/_Funkystation/StationEvents/Events/FaultyChipsRule.cs
@@ -0,0 +1,8 @@
+using Content.Server.StationEvents.Components;
+
+namespace Content.Server.StationEvents.Events;
+
+public sealed class FaultyChipsRule : StationEventSystem<FaultyChipsRuleComponent>
+{
+
+}

4067385947d2fa288fb552a32b1207a92ede9308
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2025-07-05 20:59:31 -0400

    Stasis bed cleanup and bugfixes. (#38762)
    * Stasis bed sent to shed
    
    * Code Review
    
    * Code Review 2

--- Content.Server/Bed/BedSystem.cs
+++ Content.Server/Bed/BedSystem.cs
@@ -1,58 +1,15 @@
-// SPDX-FileCopyrightText: 2022 Flipp Syder <76629141+vulppine@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2022 Jezithyr <Jezithyr@gmail.com>
-// SPDX-FileCopyrightText: 2022 Paul Ritter <ritter.paul1@googlemail.com>
-// SPDX-FileCopyrightText: 2022 Rane <60792108+Elijahrane@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2022 metalgearsloth <comedian_vs_clown@hotmail.com>
-// SPDX-FileCopyrightText: 2022 rolfero <45628623+rolfero@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2022 themias <89101928+themias@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 AJCM-git <60196617+AJCM-git@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 Jezithyr <jezithyr@gmail.com>
-// SPDX-FileCopyrightText: 2023 deltanedas <39013340+deltanedas@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 0x6273 <0x40@keemail.me>
-// SPDX-FileCopyrightText: 2024 Aiden <aiden@djkraz.com>
-// SPDX-FileCopyrightText: 2024 Fishbait <Fishbait@git.ml>
-// SPDX-FileCopyrightText: 2024 John Space <bigdumb421@gmail.com>
-// SPDX-FileCopyrightText: 2024 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Magnus Larsen <golgothasTerror101@gmail.com>
-// SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
-// SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2024 Tayrtahn <tayrtahn@gmail.com>
-// SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
-using Content.Server.Actions;
-using Content.Server.Bed.Components;
-using Content.Server.Body.Systems;
-using Content.Server.Power.Components;
-using Content.Server.Power.EntitySystems;
 using Content.Shared.Bed;
 using Content.Shared.Bed.Components;
 using Content.Shared.Bed.Sleep;
-using Content.Shared.Body.Components;
 using Content.Shared.Buckle.Components;
 using Content.Shared.Damage;
-using Content.Shared.Emag.Systems;
 using Content.Shared.Mobs.Systems;
-using Content.Shared.Power;
-using Robust.Shared.Timing;
-using Robust.Shared.Utility;
-using Content.Shared._EinsteinEngines.Silicon.Components;
 
 namespace Content.Server.Bed
 {
     public sealed class BedSystem : SharedBedSystem
     {
         [Dependency] private readonly DamageableSystem _damageableSystem = default!;
-        [Dependency] private readonly ActionsSystem _actionsSystem = default!;
-        [Dependency] private readonly EmagSystem _emag = default!;
-        [Dependency] private readonly SleepingSystem _sleepingSystem = default!;
-        [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
         [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
 
         private EntityQuery<SleepingComponent> _sleepingQuery;
@@ -62,11 +19,6 @@ namespace Content.Server.Bed
             base.Initialize();
 
             _sleepingQuery = GetEntityQuery<SleepingComponent>();
-
-            SubscribeLocalEvent<StasisBedComponent, StrappedEvent>(OnStasisStrapped);
-            SubscribeLocalEvent<StasisBedComponent, UnstrappedEvent>(OnStasisUnstrapped);
-            SubscribeLocalEvent<StasisBedComponent, PowerChangedEvent>(OnPowerChanged);
-            SubscribeLocalEvent<StasisBedComponent, GotEmaggedEvent>(OnEmagged);
         }
 
         public override void Update(float frameTime)
@@ -86,8 +38,7 @@ namespace Content.Server.Bed
 
                 foreach (var healedEntity in strapComponent.BuckledEntities)
                 {
-                    if (_mobStateSystem.IsDead(healedEntity)
-                        || HasComp<SiliconComponent>(healedEntity)) // Goobstation
+                    if (_mobStateSystem.IsDead(healedEntity))
                         continue;
 
                     var damage = bedComponent.Damage;
@@ -97,62 +48,6 @@ namespace Content.Server.Bed
 
                     _damageableSystem.TryChangeDamage(healedEntity, damage, true, origin: uid);
                 }
-            }
-        }
-
-        private void UpdateAppearance(EntityUid uid, bool isOn)
-        {
-            _appearance.SetData(uid, StasisBedVisuals.IsOn, isOn);
-        }
-
-        private void OnStasisStrapped(Entity<StasisBedComponent> bed, ref StrappedEvent args)
-        {
-            if (!HasComp<BodyComponent>(args.Buckle) || !this.IsPowered(bed, EntityManager))
-                return;
-
-            var metabolicEvent = new ApplyMetabolicMultiplierEvent(args.Buckle, bed.Comp.Multiplier, true);
-            RaiseLocalEvent(args.Buckle, ref metabolicEvent);
-        }
-
-        private void OnStasisUnstrapped(Entity<StasisBedComponent> bed, ref UnstrappedEvent args)
-        {
-            if (!HasComp<BodyComponent>(args.Buckle) || !this.IsPowered(bed, EntityManager))
-                return;
-
-            var metabolicEvent = new ApplyMetabolicMultiplierEvent(args.Buckle, bed.Comp.Multiplier, false);
-            RaiseLocalEvent(args.Buckle, ref metabolicEvent);
-        }
-
-        private void OnPowerChanged(EntityUid uid, StasisBedComponent component, ref PowerChangedEvent args)
-        {
-            UpdateAppearance(uid, args.Powered);
-            UpdateMetabolisms(uid, component, args.Powered);
-        }
-
-        private void OnEmagged(EntityUid uid, StasisBedComponent component, ref GotEmaggedEvent args)
-        {
-            if (!_emag.CompareFlag(args.Type, EmagType.Interaction))
-                return;
-
-            if (_emag.CheckFlag(uid, EmagType.Interaction))
-                return;
-
-            // Reset any metabolisms first so they receive the multiplier correctly
-            UpdateMetabolisms(uid, component, false);
-            component.Multiplier = 1 / component.Multiplier;
-            UpdateMetabolisms(uid, component, true);
-            args.Handled = true;
-        }
-
-        private void UpdateMetabolisms(EntityUid uid, StasisBedComponent component, bool shouldApply)
-        {
-            if (!TryComp<StrapComponent>(uid, out var strap) || strap.BuckledEntities.Count == 0)
-                return;
-
-            foreach (var buckledEntity in strap.BuckledEntities)
-            {
-                var metabolicEvent = new ApplyMetabolicMultiplierEvent(buckledEntity, component.Multiplier, shouldApply);
-                RaiseLocalEvent(buckledEntity, ref metabolicEvent);
             }
         }
     }

36a0bc37194a6a475feffbc51a2478bc816c52f4
Teasq <Xerithin@gmail.com>
2025-07-17 09:03:13 +0200

    please work

--- Content.Server/_DV/CosmicCult/CosmicCultRuleSystem.cs
+++ Content.Server/_DV/CosmicCult/CosmicCultRuleSystem.cs
@@ -4,7 +4,6 @@ using Content.Server.Administration.Logs;
 using Content.Server.Antag;
 using Content.Server.Atmos.Components;
 using Content.Server.Audio;
-using Content.Goobstation.Shared.Religion; // Goobstation - Shitchap
 using Content.Server.Bible.Components;
 using Content.Server.Chat.Systems;
 using Content.Server.EUI;

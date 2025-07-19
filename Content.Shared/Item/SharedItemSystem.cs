1a38e25bcc3343ba8a6375795c8cbb5610841aee
funkystationbot <funky@funkystation.org>
2025-07-19 06:07:01 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Item/SharedItemSystem.cs
+++ Content.Shared/Item/SharedItemSystem.cs
@@ -18,6 +18,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Dia <diatomic.ge@gmail.com>
+// SPDX-FileCopyrightText: 2025 sleepyyapril <flyingkarii@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

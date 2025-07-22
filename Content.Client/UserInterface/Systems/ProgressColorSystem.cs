9bbe060a684093e95a3ac0247cda860520e04b0b
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2025-07-22 12:29:17 +0200

    Update content to remove redundant Robust math types (#38930)
    Vector3, Vector4, Matrix4, and Quaternion are now gone. This switches us over to System.Numerics where applicable.

--- Content.Client/UserInterface/Systems/ProgressColorSystem.cs
+++ Content.Client/UserInterface/Systems/ProgressColorSystem.cs
@@ -1,8 +1,3 @@
-// SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
 using System.Numerics;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;

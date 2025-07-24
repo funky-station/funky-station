c68c428438d265ae550460762b95aba571e60ece
funkystationbot <funky@funkystation.org>
2025-07-24 16:22:17 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Inventory/ServerInventorySystem.cs
+++ Content.Server/Inventory/ServerInventorySystem.cs
@@ -12,6 +12,7 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 AJCM-git <60196617+AJCM-git@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 TheSecondLord <88201625+TheSecondLord@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

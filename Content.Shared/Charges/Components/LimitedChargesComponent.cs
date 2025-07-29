6b56584154890bb1378f06c1af50cb3e920c4b80
funkystationbot <funky@funkystation.org>
2025-07-29 11:47:45 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Charges/Components/LimitedChargesComponent.cs
+++ Content.Shared/Charges/Components/LimitedChargesComponent.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2023 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 August Eymann <august.eymann@gmail.com>
 // SPDX-FileCopyrightText: 2024 marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

f5e10b9e6b75d0f0d16ea66470a4901badb81a68
funkystationbot <funky@funkystation.org>
2025-07-31 06:34:38 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Funkystation/Damage/Systems/SensitiveHearingSystem.cs
+++ Content.Shared/_Funkystation/Damage/Systems/SensitiveHearingSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 vectorassembly <vectorassembly@icloud.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Numerics;
 using Content.Shared.Alert;
 using Content.Shared.Chat;

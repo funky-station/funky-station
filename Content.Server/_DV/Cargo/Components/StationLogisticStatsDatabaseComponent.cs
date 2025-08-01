a62df9d8da68020cd18e3d52e7405e59a63d0a78
funkystationbot <funky@funkystation.org>
2025-08-01 19:22:47 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/Cargo/Components/StationLogisticStatsDatabaseComponent.cs
+++ Content.Server/_DV/Cargo/Components/StationLogisticStatsDatabaseComponent.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2024 BombasterDS <115770678+BombasterDS@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 GreyMaria <mariomister541@gmail.com>
 // SPDX-FileCopyrightText: 2025 corresp0nd <46357632+corresp0nd@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //

4758d319957d86a0ce132c2feb3891318c8ff8e5
funkystationbot <funky@funkystation.org>
2025-08-06 19:59:38 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Zombies/PendingZombieComponent.cs
+++ Content.Shared/Zombies/PendingZombieComponent.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 liltenhead <104418166+liltenhead@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Mish <bluscout78@yahoo.com>
+// SPDX-FileCopyrightText: 2025 ferynn <witchy.girl.me@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

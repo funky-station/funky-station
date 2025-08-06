54f17f9d55ee5f9474a43ed177f789ebb7431572
funkystationbot <funky@funkystation.org>
2025-08-06 17:53:53 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Clothing/EntitySystems/FireResistanceSystem.cs
+++ Content.Shared/Clothing/EntitySystems/FireResistanceSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 QueerCats <jansencheng3@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //

54eda6bd608c7a7376668d9f4a37350d0fcaa9f0
ArsCatalyst <Heartflareburst@gmail.com>
2025-08-09 18:34:42 -0400

    I found it. I think this will fix

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -143,6 +143,7 @@ namespace Content.IntegrationTests.Tests
             "Convex",
             "Relic",
             "Resort",
+            "microstation",
             "dm01-entryway"
         };
 

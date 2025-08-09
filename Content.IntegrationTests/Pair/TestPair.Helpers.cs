47db0c4c14ac9d7327d68d3efe9058b7b14a51b5
funkystationbot <funky@funkystation.org>
2025-08-09 22:56:17 +0000

    chore: Automatically update REUSE headers

--- Content.IntegrationTests/Pair/TestPair.Helpers.cs
+++ Content.IntegrationTests/Pair/TestPair.Helpers.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

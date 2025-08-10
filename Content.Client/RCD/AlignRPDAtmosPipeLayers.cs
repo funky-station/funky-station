2ca112315b709928ce7d21ad89b0c0c05d721491
funkystationbot <funky@funkystation.org>
2025-08-10 21:44:45 +0000

    chore: Automatically update REUSE headers

--- Content.Client/RCD/AlignRPDAtmosPipeLayers.cs
+++ Content.Client/RCD/AlignRPDAtmosPipeLayers.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Client.Gameplay;
 using Content.Shared.Atmos.Components;
 using Content.Shared.Atmos.EntitySystems;

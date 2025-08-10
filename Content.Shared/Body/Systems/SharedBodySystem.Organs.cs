61b645ad30095c0caecbf1a7be0b1ef52c523b52
funkystationbot <funky@funkystation.org>
2025-08-10 00:11:37 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Body/Systems/SharedBodySystem.Organs.cs
+++ Content.Shared/Body/Systems/SharedBodySystem.Organs.cs
@@ -15,6 +15,8 @@
 // SPDX-FileCopyrightText: 2024 Plykiya <58439124+Plykiya@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

f620a2e37f3c5e065d0a969df08a85b1de998271
DVDPlayerOfDiscordFame <drew.c.bergen@gmail.com>
2025-08-10 23:23:02 -0700

    Leviathan fix (#3231)
    * The Leviathan will be more forgiving to Blood-Red Users (hopefully)
    
    Shout out to ruddygreat for this suggestion. Hoping this resolves any issues of the gun knocking over jugsuits when it shouldn't.
    
    * Okay, this is maybe a fix.

--- Content.Shared/_Impstation/Weapons/Ranged/DamageOnShootSystem.cs
+++ Content.Shared/_Impstation/Weapons/Ranged/DamageOnShootSystem.cs
@@ -6,6 +6,7 @@ using Content.Shared.Popups;
 using Robust.Shared.Random;
 using Content.Shared.Throwing;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Network;
 using Robust.Shared.Timing;
 using Content.Shared.Stunnable;
 using Content.Shared.Weapons.Ranged.Systems;
@@ -14,6 +15,7 @@ namespace Content.Shared.Damage.Systems;
 
 public sealed class DamageOnShootSystem : EntitySystem
 {
+    [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
     [Dependency] private readonly DamageableSystem _damageableSystem = default!;
     [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
@@ -69,9 +71,12 @@ public sealed class DamageOnShootSystem : EntitySystem
             if (entity.Comp.PopupText != null)
                 _popupSystem.PopupClient(Loc.GetString(entity.Comp.PopupText), args.User, args.User);
 
-            // Attempt to paralyze the user after they have taken damage
-            if (_random.Prob(entity.Comp.StunChance))
-                _stun.TryParalyze(args.User, TimeSpan.FromSeconds(entity.Comp.StunSeconds), true);
+            if (_net.IsClient)
+                return;
+            {
+                if (_random.Prob(entity.Comp.StunChance))
+                    _stun.TryParalyze(args.User, TimeSpan.FromSeconds(entity.Comp.StunSeconds), true);
+            }
         }
     }
 }

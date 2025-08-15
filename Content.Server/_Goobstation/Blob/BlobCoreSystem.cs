13fc6a460c36a38a2468953ee7934b83270d513e
funkystationbot <funky@funkystation.org>
2025-08-15 04:15:26 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Goobstation/Blob/BlobCoreSystem.cs
+++ Content.Server/_Goobstation/Blob/BlobCoreSystem.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 John Space <bigdumb421@gmail.com>
 // SPDX-FileCopyrightText: 2024 fishbait <gnesse@gmail.com>
 // SPDX-FileCopyrightText: 2025 QueerCats <jansencheng3@gmail.com>
+// SPDX-FileCopyrightText: 2025 Rainbow <ev0lvkitten@gmail.com>
 // SPDX-FileCopyrightText: 2025 Skye <57879983+Rainbeon@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>

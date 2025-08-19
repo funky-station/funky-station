1ca1d45041683f8237350be149406730ec96c48b
funkystationbot <funky@funkystation.org>
2025-08-19 02:57:08 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Armor/SharedArmorSystem.cs
+++ Content.Shared/Armor/SharedArmorSystem.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2025 pa.pecherskij <pa.pecherskij@interfax.ru>
 // SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 unknown <alexander.kuepper1234@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

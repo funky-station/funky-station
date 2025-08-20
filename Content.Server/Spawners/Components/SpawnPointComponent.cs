ff896c596f81550ebb918585ea1755dbc573ec70
funkystationbot <funky@funkystation.org>
2025-08-20 04:11:49 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Spawners/Components/SpawnPointComponent.cs
+++ Content.Server/Spawners/Components/SpawnPointComponent.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2023 Tom Leys <tom@crump-leys.com>
 // SPDX-FileCopyrightText: 2024 778b <33431126+778b@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Josh Hilsberg <thejoulesberg@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

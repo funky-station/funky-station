87f5d1730513dc0287ba293811062ce8470f041e
funkystationbot <funky@funkystation.org>
2025-08-23 20:06:28 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Station/Systems/StationJobsSystem.Roundstart.cs
+++ Content.Server/Station/Systems/StationJobsSystem.Roundstart.cs
@@ -12,6 +12,7 @@
 // SPDX-FileCopyrightText: 2024 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 LordCarve <27449516+LordCarve@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Mish <bluscout78@yahoo.com>
+// SPDX-FileCopyrightText: 2025 Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //

225b7234333a9ad9fa82022b4674e36f8ea80538
funkystationbot <funky@funkystation.org>
2025-08-24 23:59:49 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Radio/RadioChannelPrototype.cs
+++ Content.Shared/Radio/RadioChannelPrototype.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2023 Slava0135 <40753025+Slava0135@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 Visne <39844191+Visne@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 chavonadelal <156101927+chavonadelal@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Ecramox <65426878+Ecramox@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2025 pa.pecherskij <pa.pecherskij@interfax.ru>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>

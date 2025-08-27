dfc44dbd9fc245e19ba16e1e19e51f71664d5ae0
funkystationbot <funky@funkystation.org>
2025-08-27 00:10:57 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Funkystation/Atmos/Components/PipeBurstComponent.cs
+++ Content.Server/_Funkystation/Atmos/Components/PipeBurstComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 QueerCats <jansencheng3@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Atmos;
 using Content.Shared.Damage;
 using Robust.Shared.Audio;

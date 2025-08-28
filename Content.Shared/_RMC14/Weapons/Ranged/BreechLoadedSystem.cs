abf87c57bf0e4c88a9d4e7828dc1d5951dea41cd
funkystationbot <funky@funkystation.org>
2025-08-28 18:05:56 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_RMC14/Weapons/Ranged/BreechLoadedSystem.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/BreechLoadedSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 jackel234 <colespayde02@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._RMC14.Weapons.Common;
 using Content.Shared.Examine;
 using Content.Shared.Interaction;
--- Content.Shared/_RMC14/Weapons/Ranged/RMCTryAmmoEjectEvent.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/RMCTryAmmoEjectEvent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 jackel234 <colespayde02@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 
 namespace Content.Shared._RMC14.Weapons.Ranged;
 

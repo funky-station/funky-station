85e545eab40895143d8866a0fffa9f94b6472a3d
funkystationbot <funky@funkystation.org>
2025-09-06 19:57:56 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Flash/FlashOverlay.cs
+++ Content.Client/Flash/FlashOverlay.cs
@@ -10,6 +10,7 @@
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 heree <79546981+hereelabs@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //

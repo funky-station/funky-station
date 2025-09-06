3fd8b1af93c020d7c510d27994277e37dfb92799
PurpleTranStar <purpletranstars@gmail.com>
2025-09-06 07:25:35 -0500

    Adds IntercomOnlyBypassComponent.cs

---
+++ Content.Shared/Radio/Components/IntercomOnlyBypassComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Radio.Components;
+
+/// <summary>
+/// Dummy component to bypass common only radio
+/// </summary>
+[RegisterComponent]
+public sealed partial class IntercomOnlyBypassComponent : Component
+{
+
+}

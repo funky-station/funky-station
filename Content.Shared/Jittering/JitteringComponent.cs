f0105e66a4ad7863052ed6e92da72c15d82c12c6
funkystationbot <funky@funkystation.org>
2025-09-06 02:22:31 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Jittering/JitteringComponent.cs
+++ Content.Shared/Jittering/JitteringComponent.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Kara <lunarautomaton6@gmail.com>
+// SPDX-FileCopyrightText: 2025 DevilishMilk <michaellapjr@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

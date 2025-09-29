b0886eb68d9a7ac7a775686626a6c92a1f7d3267
funkystationbot <funky@funkystation.org>
2025-09-29 18:29:07 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Starlight/Actions/Stasis/StasisFrozenSystem.cs
+++ Content.Server/_Starlight/Actions/Stasis/StasisFrozenSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SigmaTheDragon <162711378+SigmaTheDragon@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._Starlight.Actions.Stasis;
 
 namespace Content.Server._Starlight.Actions.Stasis;
--- Content.Shared/_Starlight/Actions/Stasis/SharedStasisFrozenSystem.cs
+++ Content.Shared/_Starlight/Actions/Stasis/SharedStasisFrozenSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SigmaTheDragon <162711378+SigmaTheDragon@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.ActionBlocker;
 using Content.Shared.Emoting;
 using Content.Shared.Interaction.Events;

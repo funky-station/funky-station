7a5bb809a6efbcf533e863981e9348e22580d5fe
funkystationbot <funky@funkystation.org>
2025-10-01 21:31:49 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Communications/CommunicationsConsoleComponent.cs
+++ Content.Server/Communications/CommunicationsConsoleComponent.cs
@@ -25,6 +25,7 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Justin Pfeifler <jrpl101998@gmail.com>
 // SPDX-FileCopyrightText: 2024 avery <51971268+graevy@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 DevilishMilk <michaellapjr@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

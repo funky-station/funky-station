99cfe48c91ecc3cd2b49fc0ce6a75e911ff6c89c
funkystationbot <funky@funkystation.org>
2025-10-03 22:23:39 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Prayer/PrayerSystem.cs
+++ Content.Server/Prayer/PrayerSystem.cs
@@ -12,6 +12,7 @@
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2024 Tayrtahn <tayrtahn@gmail.com>
 // SPDX-FileCopyrightText: 2024 Voomra <dimon550@gmail.com>
+// SPDX-FileCopyrightText: 2025 Pile of Oxides <pileofoxides@posteo.de>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

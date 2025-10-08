9546431df65fbbcd3c5983992fcecd947b26e810
McBosserson <mcbosserson@hotmail.com>
2025-10-08 01:59:01 -0400

    Reapply "Port Pressure Relief Valve (#1458)" (#1470)
    This reverts commit b898bd817d989c981e30058aacc3567dcd8b353c.

---
+++ Content.Client/Atmos/EntitySystems/GasPressureRegulatorSystem.cs
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2025 ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Atmos.EntitySystems;
+using Content.Shared.Atmos.Piping.Binary.Components;
+
+namespace Content.Client.Atmos.EntitySystems;
+
+/// <summary>
+/// Represents the client system responsible for managing and updating the gas pressure regulator interface.
+/// Inherits from the shared system <see cref="SharedGasPressureRegulatorSystem"/>.
+/// </summary>
+public sealed partial class GasPressureRegulatorSystem : SharedGasPressureRegulatorSystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GasPressureRegulatorComponent, AfterAutoHandleStateEvent>(OnValveUpdate);
+    }
+
+    private void OnValveUpdate(Entity<GasPressureRegulatorComponent> ent, ref AfterAutoHandleStateEvent args)
+    {
+        UpdateUi(ent);
+    }
+
+    protected override void UpdateUi(Entity<GasPressureRegulatorComponent> ent)
+    {
+        if (UserInterfaceSystem.TryGetOpenUi(ent.Owner, GasPressureRegulatorUiKey.Key, out var bui))
+        {
+            bui.Update();
+        }
+    }
+}

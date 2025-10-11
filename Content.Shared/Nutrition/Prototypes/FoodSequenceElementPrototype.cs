f68be9649ce331a02d4504910f805563c5c05d9d
funkystationbot <funky@funkystation.org>
2025-10-11 13:34:15 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Nutrition/Prototypes/FoodSequenceElementPrototype.cs
+++ Content.Shared/Nutrition/Prototypes/FoodSequenceElementPrototype.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 Toaster <mrtoastymyroasty@gmail.com>
 // SPDX-FileCopyrightText: 2025 pa.pecherskij <pa.pecherskij@interfax.ru>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //

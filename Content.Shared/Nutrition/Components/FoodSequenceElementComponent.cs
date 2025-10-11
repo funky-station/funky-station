bd12c20f784690031560babdf3753c5f9306bd2f
funkystationbot <funky@funkystation.org>
2025-10-11 14:43:01 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Nutrition/Components/FoodSequenceElementComponent.cs
+++ Content.Shared/Nutrition/Components/FoodSequenceElementComponent.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Ed <96445749+TheShuEd@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 Toaster <mrtoastymyroasty@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

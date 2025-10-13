b19db5fd87226bc602560bd3dde5efde38fb4ced
Tojo <32783144+Alecksohs@users.noreply.github.com>
2025-10-12 19:28:47 -0500

    Typing Indicators now have separate Emote + LOOC icons (#1665)
    Co-authored-by: funkystationbot <funky@funkystation.org>

--- Content.Shared/Chat/TypingIndicator/TypingIndicatorVisuals.cs
+++ Content.Shared/Chat/TypingIndicator/TypingIndicatorVisuals.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2022 Alex Evgrashin <aevgrashin@yandex.ru>
 // SPDX-FileCopyrightText: 2025 88tv <131759102+88tv@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Tojo <32783144+Alecksohs@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 lzk <124214523+lzk228@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
@@ -9,14 +10,17 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared.Chat.TypingIndicator;
 
+// FUNKYSTATION EDIT - ADD CHATTYPE AND OVERRIDEPROTOTYPE
 [Serializable, NetSerializable]
 public enum TypingIndicatorVisuals : byte
 {
-    State
+    State,
+    ChatType,
+    OverrideIndicatorPrototype
 }
 
 [Serializable]
 public enum TypingIndicatorLayers : byte
 {
-    Base
+    Base,
 }

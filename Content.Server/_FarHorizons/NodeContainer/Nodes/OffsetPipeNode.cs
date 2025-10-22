67cad189253e9cf4c9b7c8f53f1460501901715d
jhrushbe <capnmerry@gmail.com>
2025-10-21 22:50:40 -0500

    Bluetooth pipes v1

---
+++ Content.Server/_FarHorizons/NodeContainer/Nodes/OffsetPipeNode.cs
@@ -0,0 +1,88 @@
+using Content.Server.NodeContainer.Nodes;
+using Content.Shared.Atmos;
+using Content.Shared.NodeContainer;
+using Robust.Shared.Map.Components;
+
+namespace Content.Server._FarHorizons.NodeContainer.Nodes;
+
+/// <summary>
+///     Connects with other <see cref="PipeNode"/>s whose <see cref="PipeDirection"/>
+///     and <see cref="CurrentPipeLayer"/> correctly correspond, but at a distance.
+/// </summary>
+/// This is kinda just a nasty copy of <see cref="PipeNode"/>, but the LinkableNodesInDirection() 
+/// and PipesInDirection() methods had too restricted of access.
+[DataDefinition]
+[Virtual]
+public partial class OffsetPipeNode : PipeNode
+{
+    // Described in terms of directions to make YML easier to understand
+    [DataField]
+    public int OffsetEast = 0;
+    [DataField]
+    public int OffsetNorth = 0;
+
+    public override IEnumerable<Node> GetReachableNodes(TransformComponent xform,
+            EntityQuery<NodeContainerComponent> nodeQuery,
+            EntityQuery<TransformComponent> xformQuery,
+            MapGridComponent? grid,
+            IEntityManager entMan)
+    {
+
+        if (!xform.Anchored || grid == null)
+            yield break;
+
+        var pos = grid.TileIndicesFor(xform.Coordinates);
+
+        for (var i = 0; i < PipeDirectionHelpers.PipeDirections; i++)
+        {
+            var pipeDir = (PipeDirection)(1 << i);
+
+            if (!CurrentPipeDirection.HasDirection(pipeDir))
+                continue;
+
+            foreach (var pipe in OffsetLinkableNodesInDirection(pos, pipeDir, grid, nodeQuery))
+            {
+                yield return pipe;
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Gets the pipes that can connect to us from entities on the tile or adjacent/distant in a direction.
+    /// </summary>
+    private IEnumerable<PipeNode> OffsetLinkableNodesInDirection(Vector2i pos, PipeDirection pipeDir, MapGridComponent grid,
+        EntityQuery<NodeContainerComponent> nodeQuery)
+    {
+        foreach (var pipe in OffsetPipesInDirection(pos, pipeDir, grid, nodeQuery))
+        {
+            if (pipe.NodeGroupID == NodeGroupID
+                && pipe.CurrentPipeLayer == CurrentPipeLayer
+                && pipe.CurrentPipeDirection.HasDirection(pipeDir.GetOpposite()))
+            {
+                yield return pipe;
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Gets the pipes from entities on the tile adjacent/distant in a direction.
+    /// </summary>
+    protected IEnumerable<PipeNode> OffsetPipesInDirection(Vector2i pos, PipeDirection pipeDir, MapGridComponent grid,
+        EntityQuery<NodeContainerComponent> nodeQuery)
+    {
+        var OffsetVector = new Vector2i(OffsetEast, OffsetNorth);
+        var offsetPos = pos + OffsetVector.Rotate(pipeDir.ToAngle());
+
+        foreach (var entity in grid.GetAnchoredEntities(offsetPos))
+        {
+            if (!nodeQuery.TryGetComponent(entity, out var container))
+                continue;
+
+            foreach (var node in container.Nodes.Values)
+            {
+                if (node is PipeNode pipe)
+                    yield return pipe;
+            }
+        }
+    }
+}

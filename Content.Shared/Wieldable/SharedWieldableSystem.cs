03d35bf954b446ba17745ae28f30e40d3c40d707
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-10-24 09:25:42 +0200

    Fix wielding two-handed items with only one hand (#40966)
    * fix
    
    * review

--- Content.Shared/Wieldable/SharedWieldableSystem.cs
+++ Content.Shared/Wieldable/SharedWieldableSystem.cs
@@ -293,7 +293,7 @@ public abstract class SharedWieldableSystem : EntitySystem
             return false;
         }
 
-        if (_hands.CountFreeableHands((user, hands)) < component.FreeHandsRequired)
+        if (_hands.CountFreeableHands((user, hands), except: uid) < component.FreeHandsRequired)
         {
             if (!quiet)
             {

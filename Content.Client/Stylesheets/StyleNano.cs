ed785616499d01974da761aac31716dfe84b7a5b
w.xyz() <84605679+pirakaplant@users.noreply.github.com>
2025-10-25 05:54:38 +0000

    Fixed licence

--- Content.Client/Stylesheets/StyleNano.cs
+++ Content.Client/Stylesheets/StyleNano.cs
@@ -61,7 +61,7 @@
 // SPDX-FileCopyrightText: 2025 corresp0nd <46357632+corresp0nd@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 
 using System.Linq;
 using System.Numerics;

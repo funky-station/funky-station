ce9e5752e57d09e71d7f009fb012a3d2024ef2a7
funkystationbot <funky@funkystation.org>
2025-11-01 23:22:50 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Lobby/UI/Loadouts/LoadoutGroupContainer.xaml.cs
+++ Content.Client/Lobby/UI/Loadouts/LoadoutGroupContainer.xaml.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 TrixxedHeart <46364955+TrixxedBit@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 qrwas <55228920+qrwas@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

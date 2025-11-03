a54c36a10d1ffbd6add03e8e3466085b07631dd9
Terkala <appleorange64@gmail.com>
2025-11-03 00:30:32 -0600

    Bloodcult bug fix (#1832)
    Co-authored-by: funkystationbot <funky@funkystation.org>

--- Content.Shared/_Funkystation/BloodCult/Components/ShadeComponent.cs
+++ Content.Shared/_Funkystation/BloodCult/Components/ShadeComponent.cs
@@ -1,10 +1,12 @@
 // SPDX-FileCopyrightText: 2025 Skye <57879983+Rainbeon@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 // SPDX-FileCopyrightText: 2025 kbarkevich <24629810+kbarkevich@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
 
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.BloodCult.Components;
 
@@ -14,4 +16,9 @@ namespace Content.Shared.BloodCult.Components;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class ShadeComponent : Component
 {
+	/// <summary>
+	/// The soulstone that this shade was summoned from. The shade will return here on death.
+	/// </summary>
+	[DataField]
+	public EntityUid? OriginSoulstone = null;
 }

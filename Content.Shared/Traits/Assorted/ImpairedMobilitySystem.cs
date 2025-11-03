f8608b754bc34bb72d6f4bef442cede391862309
funkystationbot <funky@funkystation.org>
2025-11-03 07:31:26 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Traits/Assorted/ImpairedMobilitySystem.cs
+++ Content.Shared/Traits/Assorted/ImpairedMobilitySystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Mora <46364955+trixxedheart@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Movement.Systems;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Hands.Components;

6b9ba549a252ef0541ba79d34b50b7726354924e
funkystationbot <funky@funkystation.org>
2025-11-09 05:28:35 +0000

    chore: Automatically update REUSE headers

--- Content.Server/GameTicking/Rules/Components/BloodCultRuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/BloodCultRuleComponent.cs
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: 2025 ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 JoulesBerg <104539820+JoulesBerg@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Skye <57879983+Rainbeon@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 // SPDX-FileCopyrightText: 2025 kbarkevich <24629810+kbarkevich@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

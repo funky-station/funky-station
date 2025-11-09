3a200744652527f63e21720cc46dc2832566558b
funkystationbot <funky@funkystation.org>
2025-11-09 16:37:48 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Mech/Equipment/EntitySystems/MechGrabberSystem.cs
+++ Content.Server/Mech/Equipment/EntitySystems/MechGrabberSystem.cs
@@ -14,6 +14,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 nikthechampiongr <32041239+nikthechampiongr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 themias <89101928+themias@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

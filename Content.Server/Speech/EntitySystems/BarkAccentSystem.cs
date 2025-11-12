7b3ed88e9b28fd32af35ee7dc4365795cf5ab268
funkystationbot <funky@funkystation.org>
2025-11-12 05:09:02 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
@@ -8,6 +8,7 @@
 // SPDX-FileCopyrightText: 2022 mirrorcult <lunarautomaton6@gmail.com>
 // SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 Ed <96445749+TheShuEd@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Toaster <mrtoastymyroasty@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

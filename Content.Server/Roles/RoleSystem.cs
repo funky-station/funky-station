e44f406ec5bfbcac0e0f1177dab130f2326949ed
BeBright <98597725+bebr3ght@users.noreply.github.com>
2025-11-13 19:24:28 +0300

    MingGetBriefing -> MindGetBriefing (#41413)
    Co-authored-by: BeBright <98597725+be1bright@users.noreply.github.com>

--- Content.Server/Roles/RoleSystem.cs
+++ Content.Server/Roles/RoleSystem.cs
@@ -33,7 +33,7 @@ public sealed class RoleSystem : SharedRoleSystem
     {
         if (mindId == null)
         {
-            Log.Error($"MingGetBriefing failed for mind {mindId}");
+            Log.Error($"MindGetBriefing failed for mind {mindId}");
             return null;
         }
 
@@ -41,7 +41,7 @@ public sealed class RoleSystem : SharedRoleSystem
 
         if (mindComp is null)
         {
-            Log.Error($"MingGetBriefing failed for mind {mindId}");
+            Log.Error($"MindGetBriefing failed for mind {mindId}");
             return null;
         }
 

1ebad8fa79ee035ce0b5ab1f91585e9c2769e393
B_Kirill <153602297+B-Kirill@users.noreply.github.com>
2025-11-14 22:20:11 +1000

    Camera Map
    some small things
    
    Camera map
    
    I hope this helps
    
    Review 1
    
    Review 2
    
    Review 3
    
    some small things
    
    Review 4
    
    Review 5
    
    Colorblind mode support
    
    Review 6
    
    Change design
    
    some small things
    
    Map wire
    
    Logic fix

--- Content.Server/SurveillanceCamera/Systems/SurveillanceCameraSystem.cs
+++ Content.Server/SurveillanceCamera/Systems/SurveillanceCameraSystem.cs
@@ -35,6 +35,8 @@ public sealed class SurveillanceCameraSystem : EntitySystem
     [Dependency] private readonly DeviceNetworkSystem _deviceNetworkSystem = default!;
     [Dependency] private readonly UserInterfaceSystem _userInterface = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly SurveillanceCameraMapSystem _cameraMapSystem = default!;
 
     // Pings a surveillance camera subnet. All cameras will always respond
     // with a data message if they are on the same subnet.
@@ -301,6 +303,8 @@ public sealed class SurveillanceCameraSystem : EntitySystem
         }
 
         UpdateVisuals(camera, component);
+
+        _cameraMapSystem.UpdateCameraMarker((camera, component));
     }
 
     public void AddActiveViewer(EntityUid camera, EntityUid player, EntityUid? monitor = null, SurveillanceCameraComponent? component = null, ActorComponent? actor = null)

279f7fb7c3ee1d37d54d17a22ef7fd3ce2d7d1d4
mycobiota <154991750+mycobiota@users.noreply.github.com>
2025-11-15 12:21:51 +1000

    restored old flippo, added new "commemorative flippo" created component that shuts the flippo up when you close it flippo is now fully functional

---
+++ Content.Shared/Tools/Components/FancyLighterComponent.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Audio;
+
+namespace Content.Shared.Tools.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class FancyLighterComponent : Component
+{
+    /// <summary>
+    /// The stupid music the lighter should play when switched on.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField, AutoNetworkedField]
+    public SoundSpecifier? SoundActivate;
+}

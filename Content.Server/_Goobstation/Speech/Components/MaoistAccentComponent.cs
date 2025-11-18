feed4b622383c0a4633900a000a437a1aaeee809
DevilishMilk <michaellapjr@gmail.com>
2025-11-17 20:58:48 -0500

    fixes spdx header

--- Content.Server/_Goobstation/Speech/Components/MaoistAccentComponent.cs
+++ Content.Server/_Goobstation/Speech/Components/MaoistAccentComponent.cs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 DevilishMilk <michaellapjr@gmail.com>
+// SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 

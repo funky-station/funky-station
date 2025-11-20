aca0f148de6101ef969f1554be74f893cb2c643f
funkystationbot <funky@funkystation.org>
2025-11-20 05:33:00 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Audio/ContentAudioSystem.cs
+++ Content.Client/Audio/ContentAudioSystem.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2024 Hannah Giovanna Dawson <karakkaraz@gmail.com>
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 RogueNova1 <alexlikowski@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT
--- Content.Client/Options/UI/Tabs/AudioTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/AudioTab.xaml.cs
@@ -11,6 +11,7 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
+// SPDX-FileCopyrightText: 2025 RogueNova1 <alexlikowski@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>

e5c58a89a522d530fb82db3dc971d02af64e16a7
funkystationbot <funky@funkystation.org>
2025-11-21 20:44:27 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Funkystation/BloodCult/Prototypes/CultistSpellPrototype.cs
+++ Content.Shared/_Funkystation/BloodCult/Prototypes/CultistSpellPrototype.cs
@@ -2,7 +2,6 @@
 // SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 // SPDX-FileCopyrightText: 2025 kbarkevich <24629810+kbarkevich@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later OR MIT
 

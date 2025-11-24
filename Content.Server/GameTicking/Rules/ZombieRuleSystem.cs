6458e6de78edae60160a2ecdf37d9c55c2367d50
funkystationbot <funky@funkystation.org>
2025-11-24 15:18:20 +0000

    chore: Automatically update REUSE headers

--- Content.Server/GameTicking/Rules/ZombieRuleSystem.cs
+++ Content.Server/GameTicking/Rules/ZombieRuleSystem.cs
@@ -31,6 +31,7 @@
 // SPDX-FileCopyrightText: 2025 JoulesBerg <104539820+JoulesBerg@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 QueerCats <jansencheng3@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 ferynn <witchy.girl.me@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

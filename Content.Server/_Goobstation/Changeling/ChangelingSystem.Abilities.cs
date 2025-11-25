627805db333c376cfb3a51701d2d2ff71e179ab5
funkystationbot <funky@funkystation.org>
2025-11-25 20:07:35 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Goobstation/Changeling/ChangelingSystem.Abilities.cs
+++ Content.Server/_Goobstation/Changeling/ChangelingSystem.Abilities.cs
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2024 yglop <95057024+yglop@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Drywink <43855731+Drywink@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Drywink <hugogrethen@gmail.com>
 // SPDX-FileCopyrightText: 2025 Eris <eris@erisws.com>
 // SPDX-FileCopyrightText: 2025 Eris <erisfiregamer1@gmail.com>
--- Content.Server/_Goobstation/Changeling/ChangelingSystem.cs
+++ Content.Server/_Goobstation/Changeling/ChangelingSystem.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2024 yglop <95057024+yglop@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Drywink <43855731+Drywink@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Drywink <hugogrethen@gmail.com>
 // SPDX-FileCopyrightText: 2025 Skye <57879983+Rainbeon@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>

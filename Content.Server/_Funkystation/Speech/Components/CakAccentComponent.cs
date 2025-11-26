e2703ab3f477e175719b6db813d55923c87c36cc
funkystationbot <funky@funkystation.org>
2025-11-26 22:35:10 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Funkystation/Speech/Components/CakAccentComponent.cs
+++ Content.Server/_Funkystation/Speech/Components/CakAccentComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 W.xyz() <tptechteam@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace Content.Server.Speech.Components
 {
     [RegisterComponent]

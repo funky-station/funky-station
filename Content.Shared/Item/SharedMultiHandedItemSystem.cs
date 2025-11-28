b379b8cd1e64124da82dc900b78bb1ff427c28a5
Toaster <mrtoastymyroasty@gmail.com>
2025-11-28 17:36:19 +0530

    so many duplicated itmes, jeez

--- Content.Shared/Item/SharedMultiHandedItemSystem.cs
+++ Content.Shared/Item/SharedMultiHandedItemSystem.cs
@@ -6,18 +6,19 @@
 // SPDX-License-Identifier: MIT
 
 using Content.Shared.Hands;
-using Content.Shared.Hands.Components;
 using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Inventory.VirtualItem;
 using Content.Shared.Popups;
 using Robust.Shared.Timing;
 
 namespace Content.Shared.Item;
 
-public abstract class SharedMultiHandedItemSystem : EntitySystem
+public sealed class MultiHandedItemSystem : EntitySystem
 {
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedVirtualItemSystem _virtualItem = default!;
 
     /// <inheritdoc/>
     public override void Initialize()
@@ -28,27 +29,39 @@ public abstract class SharedMultiHandedItemSystem : EntitySystem
         SubscribeLocalEvent<MultiHandedItemComponent, GotUnequippedHandEvent>(OnUnequipped);
     }
 
-    protected abstract void OnEquipped(EntityUid uid, MultiHandedItemComponent component, GotEquippedHandEvent args);
-    protected abstract void OnUnequipped(EntityUid uid, MultiHandedItemComponent component, GotUnequippedHandEvent args);
-
-    private void OnAttemptPickup(EntityUid uid, MultiHandedItemComponent component, GettingPickedUpAttemptEvent args)
+    private void OnEquipped(Entity<MultiHandedItemComponent> ent, ref GotEquippedHandEvent args)
     {
-        if (TryComp<HandsComponent>(args.User, out var hands) && hands.CountFreeHands() >= component.HandsNeeded)
-            return;
-
-        args.Cancel();
-        if (_timing.IsFirstTimePredicted)
+        for (var i = 0; i < ent.Comp.HandsNeeded - 1; i++)
         {
-            _popup.PopupCursor(Loc.GetString("multi-handed-item-pick-up-fail",
-                ("number", component.HandsNeeded - 1), ("item", uid)), args.User);
+            _virtualItem.TrySpawnVirtualItemInHand(ent.Owner, args.User);
         }
     }
 
-    private void OnVirtualItemDeleted(EntityUid uid, MultiHandedItemComponent component, VirtualItemDeletedEvent args)
+    private void OnUnequipped(Entity<MultiHandedItemComponent> ent, ref GotUnequippedHandEvent args)
     {
-        if (args.BlockingEntity != uid)
+        _virtualItem.DeleteInHandsMatching(args.User, ent.Owner);
+    }
+
+    private void OnAttemptPickup(Entity<MultiHandedItemComponent> ent, ref GettingPickedUpAttemptEvent args)
+    {
+        if (args.Cancelled || _hands.CountFreeHands(args.User) >= ent.Comp.HandsNeeded)
             return;
 
-        _hands.TryDrop(args.User, uid);
+        args.Cancel();
+
+        if (args.ShowPopup)
+            _popup.PopupPredictedCursor(
+                Loc.GetString("multi-handed-item-pick-up-fail",
+                    ("number", ent.Comp.HandsNeeded - 1),
+                    ("item", ent.Owner)),
+                args.User);
+    }
+
+    private void OnVirtualItemDeleted(Entity<MultiHandedItemComponent> ent, ref VirtualItemDeletedEvent args)
+    {
+        if (args.BlockingEntity != ent.Owner || _timing.ApplyingState)
+            return;
+
+        _hands.TryDrop(args.User, ent.Owner);
     }
 }

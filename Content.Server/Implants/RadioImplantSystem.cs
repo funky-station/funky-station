10b3b522e2b4a714e5db571441983984f64c0c68
Toaster <mrtoastymyroasty@gmail.com>
2025-11-30 01:43:47 +0530

    uh huh

--- Content.Server/Implants/RadioImplantSystem.cs
+++ Content.Server/Implants/RadioImplantSystem.cs
@@ -1,11 +1,3 @@
-// SPDX-FileCopyrightText: 2025 Currot <carpecarrot@gmail.com>
-// SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
-using Content.Server.Radio.Components;
 using Content.Shared.Implants;
 using Content.Shared.Implants.Components;
 using Content.Shared.Radio.Components;

deb3def620e58d92e3ddb9132b38334d30b2f61d
Toaster <mrtoastymyroasty@gmail.com>
2025-11-29 22:54:52 +0530

    golly jee willikers

--- Content.Shared/Roles/JobRequirements.cs
+++ Content.Shared/Roles/JobRequirements.cs
@@ -1,20 +1,3 @@
-// SPDX-FileCopyrightText: 2022 Moony <moonheart08@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2022 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
-// SPDX-FileCopyrightText: 2022 Veritius <veritiusgaming@gmail.com>
-// SPDX-FileCopyrightText: 2023 778b <33431126+778b@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 Brandon Hu <103440971+Brandon-Huu@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 ElectroJr <leonsfriedrich@gmail.com>
-// SPDX-FileCopyrightText: 2023 Ray <vigersray@gmail.com>
-// SPDX-FileCopyrightText: 2023 deltanedas <39013340+deltanedas@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Aiden <aiden@djkraz.com>
-// SPDX-FileCopyrightText: 2024 Ed <96445749+TheShuEd@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
 using System.Diagnostics.CodeAnalysis;
 using Content.Shared.Preferences;
 using Robust.Shared.Prototypes;
@@ -34,7 +17,7 @@ public static class JobRequirements
     /// <returns>Returns true if all requirements were met or there were no requirements.</returns>
     public static bool TryRequirementsMet(
         JobPrototype job,
-        IReadOnlyDictionary<string, TimeSpan>? playTimes,
+        IReadOnlyDictionary<string, TimeSpan> playTimes,
         [NotNullWhen(false)] out FormattedMessage? reason,
         IEntityManager entManager,
         IPrototypeManager protoManager,
@@ -72,21 +55,6 @@ public static class JobRequirements
 
         return true;
     }
-
-    public static bool TryRequirementsMet(
-        ProtoId<JobPrototype> job,
-        IReadOnlyDictionary<string, TimeSpan>? playTimes,
-        [NotNullWhen(false)] out FormattedMessage? reason,
-        IEntityManager entManager,
-        IPrototypeManager protoManager,
-        HumanoidCharacterProfile? profile)
-    {
-        if (protoManager.TryIndex(job, out var jobProto))
-            return TryRequirementsMet(jobProto, playTimes, out reason, entManager, protoManager, profile);
-
-        reason = FormattedMessage.FromUnformatted("Failed to get job prototype");
-        return false;
-    }
 }
 
 /// <summary>
@@ -103,6 +71,6 @@ public abstract partial class JobRequirement
         IEntityManager entManager,
         IPrototypeManager protoManager,
         HumanoidCharacterProfile? profile,
-        IReadOnlyDictionary<string, TimeSpan>? playTimes,
+        IReadOnlyDictionary<string, TimeSpan> playTimes,
         [NotNullWhen(false)] out FormattedMessage? reason);
 }

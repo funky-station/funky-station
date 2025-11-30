b5337dcf6e05415a9d1b8e65e67447e1fef31682
Terkala <appleorange64@gmail.com>
2025-11-30 15:43:14 -0600

    Updating copyright

--- Content.Server/_Funkystation/BloodCult/EntitySystems/JuggernautBloodAbsorptionSystem.cs
+++ Content.Server/_Funkystation/BloodCult/EntitySystems/JuggernautBloodAbsorptionSystem.cs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 //
-// SPDX-License-Identifier: AGPL-3.0-or-later OR MIT
+// SPDX-License-Identifier: MIT
 
 using Content.Server.Fluids.EntitySystems;
 using Content.Shared.BloodCult;

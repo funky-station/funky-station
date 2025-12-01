bb0b7f20fe50f073fded111c606c21c50041738b
funkystationbot <funky@funkystation.org>
2025-12-01 14:01:22 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Access/UI/IdCardConsoleBoundUserInterface.cs
+++ Content.Client/Access/UI/IdCardConsoleBoundUserInterface.cs
@@ -13,6 +13,7 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <comedian_vs_clown@hotmail.com>
 // SPDX-FileCopyrightText: 2024 Kara <lunarautomaton6@gmail.com>
 // SPDX-FileCopyrightText: 2024 c4llv07e <38111072+c4llv07e@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Toaster <mrtoastymyroasty@gmail.com>
 // SPDX-FileCopyrightText: 2025 ilyamikcoder <me@ilyamikcoder.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2025 āda <ss.adasts@gmail.com>

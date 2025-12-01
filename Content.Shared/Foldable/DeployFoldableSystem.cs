91384d13bdfb94925531a3254fbcd355a1aa5840
funkystationbot <funky@funkystation.org>
2025-12-01 12:59:20 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Foldable/DeployFoldableSystem.cs
+++ Content.Shared/Foldable/DeployFoldableSystem.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2024 ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Zergologist <114537969+Chedd-Error@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

7e090e1306819af1962afec174b1c271411b2ac3
AftrLite <61218133+AftrLite@users.noreply.github.com>
2025-12-02 19:20:19 +1300

    MidroundAntag: Entropic Colossus (Cosmic Cult) (#2162)
    Co-authored-by: funkystationbot <funky@funkystation.org>

---
+++ Content.Shared/_DV/Roles/CosmicColossusRoleComponent.cs
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 AftrLite
+// SPDX-FileCopyrightText: 2025 AftrLite <61218133+AftrLite@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Janet Blackquill <uhhadd@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
+
+using Content.Shared.Roles;
+
+namespace Content.Shared._DV.Roles;
+
+/// <summary>
+///     Added to mind role entities to tag that they are using the cosmic colossus systems.
+/// </summary>
+[RegisterComponent]
+public sealed partial class CosmicColossusRoleComponent : BaseMindRoleComponent;

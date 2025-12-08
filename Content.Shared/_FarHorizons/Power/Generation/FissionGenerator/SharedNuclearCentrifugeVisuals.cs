483bea0fade03a6b315facd295360b79db0db4da
rottenheadphones <juaelwe@outlook.com>
2025-12-07 23:20:18 -0300

    [PORT] | Nuclear Generator (#2188)
    Co-authored-by: funkystationbot <funky@funkystation.org>

---
+++ Content.Shared/_FarHorizons/Power/Generation/FissionGenerator/SharedNuclearCentrifugeVisuals.cs
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2025 rottenheadphones <juaelwe@outlook.com>
+//
+// SPDX-License-Identifier: CC-BY-NC-SA-3.0
+
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._FarHorizons.Power.Generation.FissionGenerator;
+
+/// <summary>
+/// 
+/// </summary>
+[Serializable, NetSerializable]
+public enum NuclearCentrifugeVisuals
+{
+    Processing
+}
+
+/// <summary>
+/// 
+/// </summary>
+[Serializable, NetSerializable]
+public enum NuclearCentrifugeVisualLayers
+{
+    Processing
+}

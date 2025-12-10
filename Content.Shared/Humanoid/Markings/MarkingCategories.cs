ee15fd2d955110e5c8560f76e735ee1901049271
Toaster <mrtoastymyroasty@gmail.com>
2025-12-10 21:31:06 +0530

    loads of changes

--- Content.Shared/Humanoid/Markings/MarkingCategories.cs
+++ Content.Shared/Humanoid/Markings/MarkingCategories.cs
@@ -1,13 +1,18 @@
 // SPDX-FileCopyrightText: 2022 Flipp Syder <76629141+vulppine@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Fishbait <Fishbait@git.ml>
-// SPDX-FileCopyrightText: 2024 John Space <bigdumb421@gmail.com>
 // SPDX-FileCopyrightText: 2024 Rouge2t7 <81053047+Sarahon@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025 Falcon <falcon@zigtag.dev>
-// SPDX-FileCopyrightText: 2025 pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2024 ScyronX <166930367+ScyronX@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Froffy025 <78222136+Froffy025@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Froffy025 <scotttaco025@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 Sapphire <98045970+sapphirescript@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 VMSolidus <evilexecutive@gmail.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 gluesniffler <linebarrelerenthusiast@gmail.com>
 //
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Robust.Shared.Serialization;
 
@@ -22,12 +27,22 @@ namespace Content.Shared.Humanoid.Markings
         Head,
         HeadTop,
         HeadSide,
-        Eyes, // DeltaV - Add Eyes Category for IPCs
         Snout,
         Chest,
+        RightArm,
+        RightHand,
+        LeftArm,
+        LeftHand,
+        RightLeg,
+        RightFoot,
+        LeftLeg,
+        LeftFoot,
         Arms,
         Legs,
+        Groin, // Shitmed Change
         Wings, // For IPC wings porting from SimpleStation
+        Underwear,
+        Undershirt,
         Tail,
         Overlay
     }
@@ -44,18 +59,20 @@ namespace Content.Shared.Humanoid.Markings
                 HumanoidVisualLayers.Head => MarkingCategories.Head,
                 HumanoidVisualLayers.HeadTop => MarkingCategories.HeadTop,
                 HumanoidVisualLayers.HeadSide => MarkingCategories.HeadSide,
-                HumanoidVisualLayers.Eyes => MarkingCategories.Eyes, // DeltaV - Add Eyes Category for IPCs
                 HumanoidVisualLayers.Snout => MarkingCategories.Snout,
+                HumanoidVisualLayers.Undershirt => MarkingCategories.Undershirt,
+                HumanoidVisualLayers.Underwear => MarkingCategories.Underwear,
                 HumanoidVisualLayers.Chest => MarkingCategories.Chest,
-                HumanoidVisualLayers.RArm => MarkingCategories.Arms,
-                HumanoidVisualLayers.LArm => MarkingCategories.Arms,
-                HumanoidVisualLayers.RHand => MarkingCategories.Arms,
-                HumanoidVisualLayers.LHand => MarkingCategories.Arms,
-                HumanoidVisualLayers.LLeg => MarkingCategories.Legs,
-                HumanoidVisualLayers.RLeg => MarkingCategories.Legs,
-                HumanoidVisualLayers.LFoot => MarkingCategories.Legs,
-                HumanoidVisualLayers.RFoot => MarkingCategories.Legs,
-                HumanoidVisualLayers.Wings => MarkingCategories.Wings, //For IPC wings porting from SimpleStation
+                HumanoidVisualLayers.Groin => MarkingCategories.Groin, // Shitmed Change
+                HumanoidVisualLayers.RArm => MarkingCategories.RightArm,
+                HumanoidVisualLayers.LArm => MarkingCategories.LeftArm,
+                HumanoidVisualLayers.RHand => MarkingCategories.RightHand,
+                HumanoidVisualLayers.LHand => MarkingCategories.LeftHand,
+                HumanoidVisualLayers.LLeg => MarkingCategories.LeftLeg,
+                HumanoidVisualLayers.RLeg => MarkingCategories.RightLeg,
+                HumanoidVisualLayers.LFoot => MarkingCategories.LeftFoot,
+                HumanoidVisualLayers.RFoot => MarkingCategories.RightFoot,
+                HumanoidVisualLayers.Wings => MarkingCategories.Wings,
                 HumanoidVisualLayers.Tail => MarkingCategories.Tail,
                 _ => MarkingCategories.Overlay
             };

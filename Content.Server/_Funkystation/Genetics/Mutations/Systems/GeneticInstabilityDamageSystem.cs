f36b220e93dafc431af0f7002a72f3b15cb6f499
funkystationbot <funky@funkystation.org>
2025-12-13 01:20:09 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Funkystation/Genetics/Mutations/Systems/GeneticInstabilityDamageSystem.cs
+++ Content.Server/_Funkystation/Genetics/Mutations/Systems/GeneticInstabilityDamageSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server._Funkystation.Genetics.Components;
 using Robust.Shared.Timing;
 using Content.Shared.Damage;
--- Content.Server/_Funkystation/Genetics/ReagentEffects/RandomMutation.cs
+++ Content.Server/_Funkystation/Genetics/ReagentEffects/RandomMutation.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server._Funkystation.Genetics.Components;
 using Content.Server._Funkystation.Genetics.Systems;
 using Content.Shared.EntityEffects;

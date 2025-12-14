398dafc580ce9a593621ea9a4e72622fe7209572
Steve <marlumpy@gmail.com>
2025-12-14 18:44:54 -0500

    add Hemopoiesis Overdrive and Lateral Undulation mutations

---
+++ Content.Server/_Funkystation/Genetics/Mutations/Components/BloodRegenerationComponent.cs
@@ -0,0 +1,20 @@
+namespace Content.Server._Funkystation.Genetics.Mutations.Components;
+
+[RegisterComponent]
+public sealed partial class BloodRegenerationComponent : Component
+{
+    /// <summary>
+    ///     How much blood (in units) to regenerate per second.
+    /// </summary>
+    [DataField("regenRatePerSecond")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float RegenRatePerSecond = 2.0f;
+
+    /// <summary>
+    ///     The target blood level percentage (0.0 to 1.0) to passively regenerate up to.
+    ///     e.g. 0.8 = regenerate until blood is at 80% of max.
+    /// </summary>
+    [DataField("targetPercentage")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float TargetPercentage = 1.0f;
+}

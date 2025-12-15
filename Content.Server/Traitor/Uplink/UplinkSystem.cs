20fd9cc64772e3161f1e9fd97d6e9a5b7fb1000f
funkystationbot <funky@funkystation.org>
2025-12-15 15:23:30 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Traitor/Uplink/UplinkSystem.cs
+++ Content.Server/Traitor/Uplink/UplinkSystem.cs
@@ -21,6 +21,7 @@
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tadeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 Terkala <appleorange64@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2025 terkala <appleorange64@gmail.com>
 //

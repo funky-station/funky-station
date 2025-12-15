c9426f94c7eead15a96a2817a6b138c3f7da7806
funkystationbot <funky@funkystation.org>
2025-12-15 01:45:08 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Light/RoofOverlay.cs
+++ Content.Client/Light/RoofOverlay.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

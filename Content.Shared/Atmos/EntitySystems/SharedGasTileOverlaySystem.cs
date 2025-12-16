6923cc5bcd1c6dcb6bcf5948a7f3d823c3ccdda6
YaraaraY <158123176+YaraaraY@users.noreply.github.com>
2025-12-16 18:38:31 -0300

    i'm still fucking stupid

--- Content.Shared/Atmos/EntitySystems/SharedGasTileOverlaySystem.cs
+++ Content.Shared/Atmos/EntitySystems/SharedGasTileOverlaySystem.cs
@@ -23,136 +23,136 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Atmos.EntitySystems
+{
+    public abstract class SharedGasTileOverlaySystem : EntitySystem
     {
-        public abstract class SharedGasTileOverlaySystem : EntitySystem
+        private const float TempAtMinHeatDistortion = 325.0f;
+        private const float TempAtMaxHeatDistortion = 1000.0f;
+        private const float HeatDistortionSlope = 1.0f / (TempAtMaxHeatDistortion - TempAtMinHeatDistortion);
+        private const float HeatDistortionIntercept = -TempAtMinHeatDistortion * HeatDistortionSlope;
+
+        public const byte ChunkSize = 8;
+        protected float AccumulatedFrameTime;
+        protected bool PvsEnabled;
+
+        [Dependency] protected readonly IPrototypeManager ProtoMan = default!;
+
+        /// <summary>
+        ///     array of the ids of all visible gases.
+        /// </summary>
+        public int[] VisibleGasId = default!;
+
+        public override void Initialize()
         {
-            private const float TempAtMinHeatDistortion = 325.0f;
-            private const float TempAtMaxHeatDistortion = 1000.0f;
-            private const float HeatDistortionSlope = 1.0f / (TempAtMaxHeatDistortion - TempAtMinHeatDistortion);
-            private const float HeatDistortionIntercept = -TempAtMinHeatDistortion * HeatDistortionSlope;
+            base.Initialize();
+            SubscribeLocalEvent<GasTileOverlayComponent, ComponentGetState>(OnGetState);
 
-            public const byte ChunkSize = 8;
-            protected float AccumulatedFrameTime;
-            protected bool PvsEnabled;
+            List<int> visibleGases = new();
 
-            [Dependency] protected readonly IPrototypeManager ProtoMan = default!;
-
-            /// <summary>
-            ///     array of the ids of all visible gases.
-            /// </summary>
-            public int[] VisibleGasId = default!;
-
-            public override void Initialize()
+            for (var i = 0; i < Atmospherics.TotalNumberOfGases; i++)
             {
-                base.Initialize();
-                SubscribeLocalEvent<GasTileOverlayComponent, ComponentGetState>(OnGetState);
-
-                List<int> visibleGases = new();
-
-                for (var i = 0; i < Atmospherics.TotalNumberOfGases; i++)
-                {
-                    var gasPrototype = ProtoMan.Index<GasPrototype>(i.ToString());
-                    if (!string.IsNullOrEmpty(gasPrototype.GasOverlayTexture) || !string.IsNullOrEmpty(gasPrototype.GasOverlaySprite) && !string.IsNullOrEmpty(gasPrototype.GasOverlayState))
-                        visibleGases.Add(i);
-                }
-
-                VisibleGasId = visibleGases.ToArray();
+                var gasPrototype = ProtoMan.Index<GasPrototype>(i.ToString());
+                if (!string.IsNullOrEmpty(gasPrototype.GasOverlayTexture) || !string.IsNullOrEmpty(gasPrototype.GasOverlaySprite) && !string.IsNullOrEmpty(gasPrototype.GasOverlayState))
+                    visibleGases.Add(i);
             }
 
-            private void OnGetState(EntityUid uid, GasTileOverlayComponent component, ref ComponentGetState args)
+            VisibleGasId = visibleGases.ToArray();
+        }
+
+        private void OnGetState(EntityUid uid, GasTileOverlayComponent component, ref ComponentGetState args)
+        {
+            if (PvsEnabled && !args.ReplayState)
+                return;
+
+            // Should this be a full component state or a delta-state?
+            if (args.FromTick <= component.CreationTick || args.FromTick <= component.ForceTick)
             {
-                if (PvsEnabled && !args.ReplayState)
-                    return;
-
-                // Should this be a full component state or a delta-state?
-                if (args.FromTick <= component.CreationTick || args.FromTick <= component.ForceTick)
-                {
-                    args.State = new GasTileOverlayState(component.Chunks);
-                    return;
-                }
-
-                var data = new Dictionary<Vector2i, GasOverlayChunk>();
-                foreach (var (index, chunk) in component.Chunks)
-                {
-                    if (chunk.LastUpdate >= args.FromTick)
-                        data[index] = chunk;
-                }
-
-                args.State = new GasTileOverlayDeltaState(data, new(component.Chunks.Keys));
+                args.State = new GasTileOverlayState(component.Chunks);
+                return;
             }
 
-            public static Vector2i GetGasChunkIndices(Vector2i indices)
+            var data = new Dictionary<Vector2i, GasOverlayChunk>();
+            foreach (var (index, chunk) in component.Chunks)
             {
-                return new((int) MathF.Floor((float) indices.X / ChunkSize), (int) MathF.Floor((float) indices.Y / ChunkSize));
+                if (chunk.LastUpdate >= args.FromTick)
+                    data[index] = chunk;
             }
 
-            [Serializable, NetSerializable]
-            public readonly struct GasOverlayData : IEquatable<GasOverlayData>
+            args.State = new GasTileOverlayDeltaState(data, new(component.Chunks.Keys));
+        }
+
+        public static Vector2i GetGasChunkIndices(Vector2i indices)
+        {
+            return new((int) MathF.Floor((float) indices.X / ChunkSize), (int) MathF.Floor((float) indices.Y / ChunkSize));
+        }
+
+        [Serializable, NetSerializable]
+        public readonly struct GasOverlayData : IEquatable<GasOverlayData>
+        {
+            [ViewVariables]
+            public readonly byte FireState;
+
+            [ViewVariables]
+            public readonly byte[] Opacity;
+
+            [ViewVariables]
+            public readonly byte FireType;
+
+            [ViewVariables]
+            public readonly float Temperature;
+
+            // TODO change fire color based on temps
+            // But also: dont dirty on a 0.01 kelvin change in temperatures.
+            // Either have a temp tolerance, or map temperature -> byte levels
+
+            public GasOverlayData(byte fireState, byte type, byte[] opacity, float temperature)
             {
-                [ViewVariables]
-                public readonly byte FireState;
+                FireState = fireState;
+                FireType = type;
+                Opacity = opacity;
+                Temperature = temperature;
+            }
 
-                [ViewVariables]
-                public readonly byte[] Opacity;
+            public bool Equals(GasOverlayData other)
+            {
+                if (FireState != other.FireState)
+                    return false;
 
-                [ViewVariables]
-                public readonly byte FireType;
+                if (Opacity?.Length != other.Opacity?.Length)
+                    return false;
 
-                [ViewVariables]
-                public readonly float Temperature;
-
-                // TODO change fire color based on temps
-                // But also: dont dirty on a 0.01 kelvin change in temperatures.
-                // Either have a temp tolerance, or map temperature -> byte levels
-
-                public GasOverlayData(byte fireState, byte type, byte[] opacity, float temperature)
+                if (Opacity != null && other.Opacity != null)
                 {
-                    FireState = fireState;
-                    FireType = type;
-                    Opacity = opacity;
-                    Temperature = temperature;
-                }
-
-                public bool Equals(GasOverlayData other)
-                {
-                    if (FireState != other.FireState)
-                        return false;
-
-                    if (Opacity?.Length != other.Opacity?.Length)
-                        return false;
-
-                    if (Opacity != null && other.Opacity != null)
+                    for (var i = 0; i < Opacity.Length; i++)
                     {
-                        for (var i = 0; i < Opacity.Length; i++)
-                        {
-                            if (Opacity[i] != other.Opacity[i])
-                                return false;
-                        }
+                        if (Opacity[i] != other.Opacity[i])
+                            return false;
                     }
-
-                    if (!MathHelper.CloseToPercent(Temperature, other.Temperature))
-                        return false;
-
-                    return true;
                 }
-            }
 
-            /// <summary>
-            /// Calculate the heat distortion from a temperature.
-            /// Returns 0.0f below TempAtMinHeatDistortion and 1.0f above TempAtMaxHeatDistortion.
-            /// </summary>
-            /// <param name="temp"></param>
-            /// <returns></returns>
-            public static float GetHeatDistortionStrength(float temp)
-            {
-                return MathHelper.Clamp01(temp * HeatDistortionSlope + HeatDistortionIntercept);
-            }
+                if (!MathHelper.CloseToPercent(Temperature, other.Temperature))
+                    return false;
 
-            [Serializable, NetSerializable]
-            public sealed class GasOverlayUpdateEvent : EntityEventArgs
-            {
-                public Dictionary<NetEntity, List<GasOverlayChunk>> UpdatedChunks = new();
-                public Dictionary<NetEntity, HashSet<Vector2i>> RemovedChunks = new();
+                return true;
             }
         }
+
+        /// <summary>
+        /// Calculate the heat distortion from a temperature.
+        /// Returns 0.0f below TempAtMinHeatDistortion and 1.0f above TempAtMaxHeatDistortion.
+        /// </summary>
+        /// <param name="temp"></param>
+        /// <returns></returns>
+        public static float GetHeatDistortionStrength(float temp)
+        {
+            return MathHelper.Clamp01(temp * HeatDistortionSlope + HeatDistortionIntercept);
+        }
+
+        [Serializable, NetSerializable]
+        public sealed class GasOverlayUpdateEvent : EntityEventArgs
+        {
+            public Dictionary<NetEntity, List<GasOverlayChunk>> UpdatedChunks = new();
+            public Dictionary<NetEntity, HashSet<Vector2i>> RemovedChunks = new();
+        }
     }
+}

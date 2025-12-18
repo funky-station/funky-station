da26040be72eec64bd59aa375e74946467bc2ee0
funkystationbot <funky@funkystation.org>
2025-12-18 04:53:30 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Medical/BedWheelsComponent.cs
+++ Content.Shared/Medical/BedWheelsComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 otokonoko-dev <248204705+otokonoko-dev@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.Manager.Attributes;

016691c5a51c67faeb09367015c76a3c1d481a76
funkystationbot <funky@funkystation.org>
2025-12-19 23:41:27 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Mono/Traits/BrittleBonesSystem.cs
+++ Content.Server/_Mono/Traits/BrittleBonesSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 SaffronFennec <firefoxwolf2020@protonmail.com>
+// SPDX-FileCopyrightText: 2025 YaraaraY <158123176+YaraaraY@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later AND MIT

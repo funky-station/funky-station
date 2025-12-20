2cfb40f2a4e444893dac638c91bfd247d30892df
funkystationbot <funky@funkystation.org>
2025-12-20 18:23:10 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Atmos/Components/GridFireLightsComponent.cs
+++ Content.Server/Atmos/Components/GridFireLightsComponent.cs
@@ -1,4 +1,8 @@
-﻿namespace Content.Server.Atmos.Components
+// SPDX-FileCopyrightText: 2025 YaraaraY <158123176+YaraaraY@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Server.Atmos.Components
 {
     [RegisterComponent]
     public sealed partial class GridFireLightsComponent : Component

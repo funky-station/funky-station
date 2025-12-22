4aa55329c9148f2b2f05f2b0936f31f74c149963
funkystationbot <funky@funkystation.org>
2025-12-22 17:50:31 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Administration/Systems/BufferingSystem.cs
+++ Content.Client/Administration/Systems/BufferingSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Pok <113675512+Pok27@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Administration.Systems;
 
 namespace Content.Client.Administration.Systems;

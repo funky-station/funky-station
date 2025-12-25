6bdbf755ffb5ab1422f44d1fd1bb781812092fe5
YaraaraY <158123176+YaraaraY@users.noreply.github.com>
2025-12-25 00:59:39 -0300

    really hope this works

--- Content.Server.Database/Migrations/Sqlite/20251225035645_DBJobAlternateTitle.cs
+++ Content.Server.Database/Migrations/Sqlite/20251225035645_DBJobAlternateTitle.cs
@@ -1,15 +1,11 @@
-// SPDX-FileCopyrightText: 2025 YaraaraY <158123176+YaraaraY@users.noreply.github.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Content.Server.Database.Migrations.Sqlite
 {
     /// <inheritdoc />
-    public partial class AddAltJobTitles : Migration
+    public partial class DBJobAlternateTitle : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)

00bf78b7ddcfa89b06dde67bf25fbe411ca8165a
funkystationbot <funky@funkystation.org>
2025-12-25 02:22:19 +0000

    chore: Automatically update REUSE headers

--- Content.Server.Database/Migrations/Postgres/20251225005224_AddAltJobTitles.cs
+++ Content.Server.Database/Migrations/Postgres/20251225005224_AddAltJobTitles.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+// SPDX-FileCopyrightText: 2025 YaraaraY <158123176+YaraaraY@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 

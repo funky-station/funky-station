5149aaa14fff31ac9387b0479439813a80401ba7
MaiaArai <158123176+YaraaraY@users.noreply.github.com>
2025-12-28 06:30:45 -0300

    Cosmetic Job Titles (#2407)
    Co-authored-by: funkystationbot <funky@funkystation.org>
    Co-authored-by: taydeo <td12233a@gmail.com>

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -40,6 +40,7 @@
 // SPDX-FileCopyrightText: 2025 GreyMario <mariomister541@gmail.com>
 // SPDX-FileCopyrightText: 2025 JORJ949 <159719201+JORJ949@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tay <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 YaraaraY <158123176+YaraaraY@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 ferynn <117872973+ferynn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
@@ -244,30 +245,17 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
         var gearEquippedEv = new StartingGearEquippedEvent(entity.Value);
         RaiseLocalEvent(entity.Value, ref gearEquippedEv);
 
+        JobAlternateTitlePrototype? altTitle = null;
+        if (profile != null && prototype != null && profile.JobAlternateTitles.TryGetValue(prototype.ID, out var altId))
+            _prototypeManager.TryIndex(altId, out altTitle);
+
         if (profile != null)
         {
             if (prototype != null)
-            {
-                if (nameOverride != null)
-                {
-                    SetPdaAndIdCardData(entity.Value, nameOverride, prototype, station);
-                    _metaSystem.SetEntityName(entity.Value, nameOverride);
-                }
-                else
-                {
-                    SetPdaAndIdCardData(entity.Value, profile.Name, prototype, station);
-                    _metaSystem.SetEntityName(entity.Value, profile.Name);
-                }
-            }
-            else
-            {
-                // we still want to set their name appropriately
-                _metaSystem.SetEntityName(entity.Value, profile.Name);
-            }
-
+                SetPdaAndIdCardData(entity.Value, profile.Name, prototype, station, altTitle);
 
             _humanoidSystem.LoadProfile(entity.Value, profile);
-
+            _metaSystem.SetEntityName(entity.Value, profile.Name);
             if (profile.FlavorText != "" && _configurationManager.GetCVar(CCVars.FlavorText))
             {
                 AddComp<DetailExaminableComponent>(entity.Value).Content = profile.FlavorText;
@@ -297,7 +285,7 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
     /// <param name="characterName">Character name to use for the ID.</param>
     /// <param name="jobPrototype">Job prototype to use for the PDA and ID.</param>
     /// <param name="station">The station this player is being spawned on.</param>
-    public void SetPdaAndIdCardData(EntityUid entity, string characterName, JobPrototype jobPrototype, EntityUid? station)
+    public void SetPdaAndIdCardData(EntityUid entity, string characterName, JobPrototype jobPrototype, EntityUid? station, JobAlternateTitlePrototype? jobAltTitle = null)
     {
         if (!InventorySystem.TryGetSlotEntity(entity, "id", out var idUid))
             return;
@@ -310,7 +298,10 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
             return;
 
         _cardSystem.TryChangeFullName(cardId, characterName, card);
-        _cardSystem.TryChangeJobTitle(cardId, jobPrototype.LocalizedName, card);
+        if (jobAltTitle != null)
+            _cardSystem.TryChangeJobTitle(cardId, jobAltTitle.LocalizedName, card);
+        else
+            _cardSystem.TryChangeJobTitle(cardId, jobPrototype.LocalizedName, card);
 
         if (_prototypeManager.TryIndex(jobPrototype.Icon, out var jobIcon))
             _cardSystem.TryChangeJobIcon(cardId, jobIcon, card);

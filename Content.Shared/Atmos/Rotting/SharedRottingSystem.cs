a7405916f2aa8bf627d1e257922f3b995a4011b6
funkystationbot <funky@funkystation.org>
2025-12-29 12:01:51 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Atmos/Rotting/SharedRottingSystem.cs
+++ Content.Shared/Atmos/Rotting/SharedRottingSystem.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Kara <lunarautomaton6@gmail.com>
 // SPDX-FileCopyrightText: 2024 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 ferynn <witchy.girl.me@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

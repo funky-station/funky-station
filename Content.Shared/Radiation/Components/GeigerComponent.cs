90c2e7cfde888cc1167c46d35aaec091f48c1c55
funkystationbot <funky@funkystation.org>
2026-01-01 22:52:41 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Radiation/Components/GeigerComponent.cs
+++ Content.Shared/Radiation/Components/GeigerComponent.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 Pieter-Jan Briers <pieterjan.briers@gmail.com>
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 YaraaraY <158123176+YaraaraY@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
 //
 // SPDX-License-Identifier: MIT

2c473bcde261706d2b1b92762c25e4875b8bda0b
funkystationbot <funky@funkystation.org>
2026-01-05 05:25:09 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Alex Evgrashin <aevgrashin@yandex.ru>
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 W.xyz() <tptechteam@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

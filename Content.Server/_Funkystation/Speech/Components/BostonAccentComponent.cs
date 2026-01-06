998c281405c7c523819283a437b37351927a045b
funkystationbot <funky@funkystation.org>
2026-01-06 05:12:23 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Funkystation/Speech/Components/BostonAccentComponent.cs
+++ Content.Server/_Funkystation/Speech/Components/BostonAccentComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 W.xyz() <tptechteam@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace Content.Server.Speech.Components
 {
     [RegisterComponent]

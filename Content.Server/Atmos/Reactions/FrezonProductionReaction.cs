413b348287d1d6366f968b8d906d856460407829
funkystationbot <funky@funkystation.org>
2026-01-16 17:53:46 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Atmos/Reactions/FrezonProductionReaction.cs
+++ Content.Server/Atmos/Reactions/FrezonProductionReaction.cs
@@ -3,9 +3,9 @@
 // SPDX-FileCopyrightText: 2023 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 Tom Leys <tom@crump-leys.com>
 // SPDX-FileCopyrightText: 2024 Jezithyr <jezithyr@gmail.com>
-// SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
 // SPDX-FileCopyrightText: 2025 marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 Steve <marlumpy@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
--- Content.Shared/Atmos/Components/GasMinerComponent.cs
+++ Content.Shared/Atmos/Components/GasMinerComponent.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Aiden <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2024 Mervill <mervills.email@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 Steve <marlumpy@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

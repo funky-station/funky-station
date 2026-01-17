9995d40ae0491b70d0097699133999d29fc0bae1
funkystationbot <funky@funkystation.org>
2026-01-17 01:55:39 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_Floof/Traits/Components/FixtureDensityModifier.cs
+++ Content.Shared/_Floof/Traits/Components/FixtureDensityModifier.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2024 fox <daytimer253@gmail.com>
+// SPDX-FileCopyrightText: 2025 ImpstationBot <irismessage+bot@protonmail.com>
 // SPDX-FileCopyrightText: 2025 sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later AND MIT

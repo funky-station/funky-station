77f7e27517b43411db8ccba034bf5aa424520fb8
funkystationbot <funky@funkystation.org>
2026-01-18 22:23:02 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Atmos/EntitySystems/PipeRestrictOverlapSystem.cs
+++ Content.Server/Atmos/EntitySystems/PipeRestrictOverlapSystem.cs
@@ -13,6 +13,7 @@
 // SPDX-FileCopyrightText: 2025 marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 TheHolyAegis <sanderkamphuis719@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 

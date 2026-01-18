53a5cb754e59e461a3ed83d931e8b3a6761daf66
funkystationbot <funky@funkystation.org>
2026-01-18 22:43:51 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/CCVar/CCVars.Atmos.cs
+++ Content.Shared/CCVar/CCVars.Atmos.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2025 VMSolidus <evilexecutive@gmail.com>
 // SPDX-FileCopyrightText: 2025 corresp0nd <46357632+corresp0nd@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 TheHolyAegis <sanderkamphuis719@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

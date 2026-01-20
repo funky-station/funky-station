5344366bc69d3f4bfaf48df58f3911f7ee5f475a
TheHolyAegis <sanderkamphuis719@gmail.com>
2026-01-20 19:30:38 +0100

    Basics made

---
+++ Content.Server/_Funkystation/Roles/Jobs/CentralCommand/Components/CCIComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Server._Funkystation.Roles.Jobs.CentralCommand.Components;
+
+/// <summary>
+/// Marker component for Central Command Intern ghost role to enable character selection
+/// </summary>
+[RegisterComponent]
+public sealed partial class CentralCommandInternComponent : Component
+{
+}
---
+++ Content.Server/_Funkystation/Roles/Jobs/CentralCommand/Components/CCISystem.cs
@@ -0,0 +1,47 @@
+﻿using Content.Server.Ghost.Roles;
+using Content.Server.Ghost.Roles.Components;
+using Content.Server.Preferences.Managers;
+using Content.Shared.Humanoid;
+
+namespace Content.Server._Funkystation.Roles.Jobs.CentralCommand.Components;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class CCISystem : EntitySystem
+{
+    [Dependency] private readonly IServerPreferencesManager _preferences = default!;
+    [Dependency] private readonly SharedHumanoidAppearanceSystem _appearance = default!;
+    [Dependency] private readonly GhostRoleSystem _ghostRole = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<CentralCommandInternComponent, TakeGhostRoleEvent>(OnTakeGhostRole);
+    }
+
+    private void OnTakeGhostRole(Entity<CentralCommandInternComponent> ent, ref TakeGhostRoleEvent args)
+    {
+        if (args.TookRole)
+            return;
+
+        var session = args.Player;
+
+        var prefs = _preferences.GetPreferences(session.UserId);
+
+        var profile = prefs.SelectProfileForJob("Passenger");
+
+        if (profile == null)
+        {
+            Log.Warning($"No valid profile found for Central Command Intern ghost role for player {session.Name}.");
+            return;
+        }
+
+        Log.Info($"Applying Central Command Intern ghost role profile for player {session.Name}.");
+
+        if (TryComp<HumanoidAppearanceComponent>(ent, out var appearance))
+        {
+            _appearance.LoadProfile(ent, profile);
+        }
+    }
+}

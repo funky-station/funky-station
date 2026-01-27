6fe9b4082442b1887d9e3302486b7b19fe147471
funkystationbot <funky@funkystation.org>
2026-01-27 10:34:25 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/CosmicCult/CultifyBorgCommand.cs
+++ Content.Server/_DV/CosmicCult/CultifyBorgCommand.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 AftrLite <61218133+AftrLite@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;

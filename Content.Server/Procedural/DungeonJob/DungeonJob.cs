2e8fa2dce55d8809da4fa7ec14f4ada0e68747b1
funkystationbot <funky@funkystation.org>
2026-01-31 15:00:38 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Procedural/DungeonJob/DungeonJob.cs
+++ Content.Server/Procedural/DungeonJob/DungeonJob.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 Terkala <appleorange64@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

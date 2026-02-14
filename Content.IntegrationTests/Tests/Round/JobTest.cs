602cabc89f36db3f39d0b2a463fce49018254616
funkystationbot <funky@funkystation.org>
2026-02-14 22:42:32 +0000

    chore: Automatically update REUSE headers

--- Content.IntegrationTests/Tests/Round/JobTest.cs
+++ Content.IntegrationTests/Tests/Round/JobTest.cs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2024 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Tayrtahn <tayrtahn@gmail.com>
+// SPDX-FileCopyrightText: 2025 Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 willow <willowzeta632146@proton.me>
 //
 // SPDX-License-Identifier: MIT
 

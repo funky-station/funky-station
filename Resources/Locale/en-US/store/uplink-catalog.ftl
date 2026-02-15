yesteryuri/imp-station-14

Syndicate Arms Race (#2575)
* ouroboros

* I can't test this yet please help

* empty marker comp

* Boop

* }soap

* Update ouroborosbolt.yml

* Bubkiss

* The Rest

Lotta stuffs here

* Update attributions.yml

I don't know what's going on with the AudioSystems in the Food component, but I know how to fix this at least.

* Update FoodReagentExtractorSystem.cs

* There's the promblem

* Last minute change

I feel better about these numbers for the Svalinn Thingy.

* Resolving Conflicts

* Correct Minor But Prevalent Spelling Mistake

Amazing how long you can go without realizing something like this. Rather than going through and correcting every every instance I've just correct the player-facing ones. If you want to hold a gun to my head metaphorically OR literally I can go do the rest tomorrow.

* my shame is forever hidden

y'know except for that one PR on Delta V that I'm not sure is getting this commit but life can't all be peaches and cream

* Descriptions Pass

Another draft on the weapon descriptions to make them shorter and punchier.

* New component pogchamp

Adding "Solution Refuel", a component that takes the refueling aspect of welders and divorces it from the Welder Component so you can have a refueling item without needing the rest of that package. I also add SolutionItemStatus to a lot of stuff so you can actually tell how much is in your guns and containers with the UI.

* Finishing Touch

one last little .ftl addition to the messages. Went the extra mile to make it open-ended so this component doesn't have to be just for weapons or tools or objects, it can remain pretty open-ended.

* Code Trim

one last polish. Just trimming unused code from the component to tidy it up. Had no effect in testing.

* Update meta.json

* Final Commit

updating my custom reagent for the upstream changes and implementing the storage sprite for the Anaconda.

* Trailing Whitespace My Mortal Enemy

I see we have these checks now too...

* Some changes

Based on balance c

Resources/Locale/en-US/store/uplink-catalog.ftl
@@ -127,7 +127,7 @@ uplink-mosin-ammo-desc = A box of 60 cartridges for the surplus rifle.
 
 #imp desc tweak
 uplink-sniper-ammo-name = Ammunition box (.60 antimateriel)
-uplink-sniper-ammo-desc = A box of 10 cartridges for the Lyndworm sniper rifle.
+uplink-sniper-ammo-desc = A box of 10 cartridges for the Lindwyrm sniper rifle and Anaconda Revolver.
 
 # Utility
 uplink-holopara-kit-name = Holoparasite Kit
@@ -267,7 +267,7 @@ uplink-ammo-bundle-desc = Reloading! Contains 4 magazines for the C-20R Gorgon, 
 
 #imp name tweaks
 uplink-sniper-bundle-name = Sniper Bundle
-uplink-sniper-bundle-desc = An inconspicuous briefcase that contains a Lyndworm, 10 spare bullets, and a convenient disguise.
+uplink-sniper-bundle-desc = An inconspicuous briefcase that contains a Lindwyrm, 10 spare bullets, and a convenient disguise.
 
 #imp name tweak
 uplink-c20r-bundle-name = C-20R Gorgon Bundle

funky-station/funky-station

Resources/Locale/en-US/store/uplink-catalog.ftl
@@ -208,7 +208,7 @@ uplink-mosin-ammo-name = Ammunition box (.30 rifle)
 uplink-mosin-ammo-desc = A box of 60 cartridges for the surplus rifle.
 
 uplink-sniper-ammo-name = Ammunition box (.60 antimateriel)
-uplink-sniper-ammo-desc = A box of 10 cartridges for the Hristov sniper rifle.
+uplink-sniper-ammo-desc = A box of 10 cartridges for the Hristov sniper rifle and Anaconda Revolver.
 
 uplink-hyposhell-name = Box of hyposhells
 uplink-hyposhell-desc = A box containing four hyposhells, shotgun shells that can hold 7u of any chemical.
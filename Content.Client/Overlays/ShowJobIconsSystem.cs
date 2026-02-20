9d756f9ecbcd2edfd37d160ecd98ed7096301813
funkystationbot <funky@funkystation.org>
2026-02-20 14:40:18 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Overlays/ShowJobIconsSystem.cs
+++ Content.Client/Overlays/ShowJobIconsSystem.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Krunklehorn <42424291+Krunklehorn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 QueerCats <jansencheng3@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 

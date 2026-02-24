520567b356059b0df995e6880e159285a4498744
TrixxedHeart <46364955+TrixxedBit@users.noreply.github.com>
2026-02-24 00:23:21 -0600

    "are you stupid" me:

--- Content.Shared/Traits/Assorted/ChronicMigrainesComponent.cs
+++ Content.Shared/Traits/Assorted/ChronicMigrainesComponent.cs
@@ -4,38 +4,39 @@
 // SPDX-License-Identifier: MIT
 
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
-namespace Content.Server.Traits.Assorted;
+namespace Content.Shared.Traits.Assorted;
 
 /// <summary>
 /// Component for the chronic migraines trait.
 /// </summary>
-[RegisterComponent, Access(typeof(ChronicMigrainesSystem)), NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, AutoGenerateComponentPause]
 public sealed partial class ChronicMigrainesComponent : Component
 {
     /// <summary>
     /// Time range between migraines (min, max).
     /// Default: 8-20 minutes
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public (TimeSpan Min, TimeSpan Max) TimeBetweenMigraines = (TimeSpan.FromMinutes(8), TimeSpan.FromMinutes(20));
 
     /// <summary>
     /// Duration range for migraines (min, max).
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public (TimeSpan Min, TimeSpan Max) MigraineDuration = (TimeSpan.FromSeconds(30), TimeSpan.FromSeconds(60));
 
     /// <summary>
     /// How long the fadeout should take when migraines end (in seconds).
     /// </summary>
-    [DataField]
-    public float FadeOutDuration { get; private set; } = 0.5f;
+    [DataField, AutoNetworkedField]
+    public float FadeOutDuration = 0.5f;
 
     /// <summary>
     /// Time until next migraine.
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField, AutoNetworkedField]
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
     public TimeSpan NextMigraineTime;
 }
+

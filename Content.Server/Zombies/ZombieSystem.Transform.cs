90fa8fc8887b3f6ea0a62aea6a7df145ca8f6fc0
QueerCats <jansencheng3@gmail.com>
2026-02-24 13:31:32 +0000

    woops

--- Content.Server/Zombies/ZombieSystem.Transform.cs
+++ Content.Server/Zombies/ZombieSystem.Transform.cs
@@ -303,7 +303,7 @@ public sealed partial class ZombieSystem
             _npc.WakeNPC(target, htn);
         }
 
-        if (!hasMind) //this specific component gives build test trouble so pop off, ig
+        if (!HasComp<GhostRoleMobSpawnerComponent>(target) && !hasMind) //this specific component gives build test trouble so pop off, ig
         {
             //yet more hardcoding. Visit zombie.ftl for more information.
             var ghostRole = EnsureComp<GhostRoleComponent>(target);

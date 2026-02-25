a2bb6efbe10d6bbd557c5a3a2c3bb30eafd9e69d
funkystationbot <funky@funkystation.org>
2026-02-25 13:01:38 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Research/Systems/ResearchSystem.cs
+++ Content.Server/Research/Systems/ResearchSystem.cs
@@ -8,9 +8,10 @@
 // SPDX-FileCopyrightText: 2022 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2022 mirrorcult <lunarautomaton6@gmail.com>
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 MeowVal <meowval@catkatnya.com>
 //
 // SPDX-License-Identifier: MIT
 

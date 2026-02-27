3f2ad76672feeebe0009e9f0ad9196059137448d
beck-thompson <107373427+beck-thompson@users.noreply.github.com>
2026-02-27 14:35:30 -0800

    Allow documents to be created from text files (#2776)
    Co-authored-by: funkystationbot <funky@funkystation.org>

---
+++ Content.Shared/_Starlight/DocumentManager/PreWrittenDocumentManager.cs
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: 2026 beck-thompson <beck314159@hotmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Robust.Shared.ContentPack;
+using Robust.Shared.Utility;
+
+namespace Content.Shared._Starlight.DocumentManager;
+
+/// <summary>
+///     Preload text documents that are written from text files.
+///     This is done because FTL files are really hard to write large documents in.
+/// </summary>
+public sealed class PreWrittenDocumentManager: IEntityEventSubscriber
+{
+    [Dependency] private readonly IResourceManager _resource = default!;
+    [Dependency] private readonly ILocalizationManager _loc = default!;
+
+    /// <summary>
+    ///     Dictionary of file name -> document contents.
+    /// </summary>
+    private Dictionary<string, string> _nameToDocument = new();
+
+    private const string DocumentsPath = "/Documents/";
+    private const string FallbackLocalization = "en-US";
+
+    private const string ValidExtension = "txt";
+
+    public async void Initialize()
+    {
+        _nameToDocument = await LoadDocuments();
+    }
+
+    /// <summary>
+    ///     Try to get contents of a document with the given name.
+    /// </summary>
+    /// <param name="name">The file name (E.g "test_1.txt").</param>
+    /// <param name="result">The text stored inside the file.</param>
+    /// <returns>If the document was found.</returns>
+    public bool TryGetDocumentContents(string name, [NotNullWhen(true)] out string? result)
+    {
+        _nameToDocument.TryGetValue(name, out result);
+
+        return result != null;
+    }
+
+    private Task<Dictionary<string, string>> LoadDocuments()
+    {
+        return Task.Run(() =>
+        {
+            var documents = new Dictionary<string, string>();
+
+            LoadFilesFromLoc(_loc.DefaultCulture?.Name, ref documents);
+            LoadFilesFromLoc(FallbackLocalization, ref documents);
+
+            return documents;
+        });
+    }
+
+    private void LoadFilesFromLoc(string? locName, ref Dictionary<string, string> documents)
+    {
+        if (locName == null)
+            return;
+
+        var path = new ResPath(DocumentsPath) / locName;
+        foreach (var file in _resource.ContentFindFiles(path))
+        {
+            if (file.Extension != ValidExtension)
+                continue;
+
+            var text = _resource.ContentFileReadAllText(file);
+
+            // no dupes!
+            DebugTools.Assert(!_nameToDocument.ContainsKey(file.Filename));
+
+            documents.TryAdd(file.Filename, text);
+        }
+    }
+}

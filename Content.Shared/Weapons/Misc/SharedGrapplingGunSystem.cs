fc9b6027c80bde8ef4a5a836a7c82cbc826c2b60
funkystationbot <funky@funkystation.org>
2026-03-02 20:55:50 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Weapons/Misc/SharedGrapplingGunSystem.cs
+++ Content.Shared/Weapons/Misc/SharedGrapplingGunSystem.cs
@@ -7,6 +7,7 @@
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 alex-infdev <185717397+alex-infdev@users.noreply.github.com>
 //
 // SPDX-License-Identifier: MIT
 

290e616b4c3a6893e4c59269a6e5b6bd3fb04b09
funkystationbot <funky@funkystation.org>
2026-03-06 15:49:19 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_EE/Supermatter/Components/SupermatterComponent.cs
+++ Content.Shared/_EE/Supermatter/Components/SupermatterComponent.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2025 corresp0nd <46357632+corresp0nd@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 MeowVal <meowval@catkatnya.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
 

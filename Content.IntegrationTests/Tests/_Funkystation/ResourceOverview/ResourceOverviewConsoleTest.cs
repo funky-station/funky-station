b18c4335b90d27c0373ea8b672645ab304e1bef5
Terkala <appleorange64@gmail.com>
2026-03-09 13:49:06 -0500

    kicking the build again, again

--- Content.IntegrationTests/Tests/_Funkystation/ResourceOverview/ResourceOverviewConsoleTest.cs
+++ Content.IntegrationTests/Tests/_Funkystation/ResourceOverview/ResourceOverviewConsoleTest.cs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Terkala <appleorange64@gmail.com>
 //
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 
 using System.Linq;
 using Content.Server.Materials;

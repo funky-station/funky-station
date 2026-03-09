2615dd3cc876866a0343297eb3eae3d4b1880e77
funkystationbot <funky@funkystation.org>
2026-03-09 16:13:51 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_Funkystation/ResourceOverview/UI/ResourceOverviewNavMapControl.cs
+++ Content.Client/_Funkystation/ResourceOverview/UI/ResourceOverviewNavMapControl.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Terkala <appleorange64@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Client.Pinpointer.UI;
 using Content.Shared._Funkystation.ResourceOverview.BUI;
 using Robust.Client.Graphics;

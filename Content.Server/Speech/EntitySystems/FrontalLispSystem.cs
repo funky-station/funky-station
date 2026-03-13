848a7e3536afdf5a5093f2709e26a66375adb338
funkystationbot <funky@funkystation.org>
2026-03-13 04:55:33 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
+++ Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2023 dahnte <70238020+dahnte@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 W.xyz() <84605679+pirakaplant@users.noreply.github.com>
 //
 // SPDX-License-Identifier: MIT
 

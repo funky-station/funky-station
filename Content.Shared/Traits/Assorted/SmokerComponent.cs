08f1217a58efb9abef586cc8f87d20afc75e7b33
HealedFoil <healedfoil@gmail.com>
2026-03-17 21:57:17 +0200

    created a working base for system

--- Content.Shared/Traits/Assorted/SmokerComponent.cs
+++ Content.Shared/Traits/Assorted/SmokerComponent.cs
@@ -8,11 +8,19 @@ public sealed partial class SmokerComponent : Component
 {
     // How frequently (in seconds) should the user smoke
     [ViewVariables(VVAccess.ReadWrite), DataField]
-    public float SmokingInterval = 60f;
+    public float SmokingInterval =60f;
 
     //the current stage of withdrawal
     [ViewVariables(VVAccess.ReadWrite), DataField]
     public int WithdrawalStage;
+
+    //how much nicotine is in the system.
+    [ViewVariables(VVAccess.ReadOnly), DataField]
+    public float CurrentNicotineLevel;
+
+    //how long since entity last smoked (in seconds)
+    [ViewVariables(VVAccess.ReadOnly), DataField]
+    public float TimeSinceSmoking;
 
 
 
---
+++ Content.Shared/Traits/Assorted/SmokerSystem.cs
@@ -0,0 +1,54 @@
+﻿using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.Popups;
+
+namespace Content.Shared.Traits.Assorted;
+
+public sealed class SmokerSystem : EntitySystem
+{
+
+    [Dependency] private SharedSolutionContainerSystem _solutionContainerSystem = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<SmokerComponent, ComponentStartup>(OnStartup);
+    }
+
+    private void OnStartup(EntityUid uid, SmokerComponent smoker, ComponentStartup args)
+    {
+        smoker.TimeSinceSmoking = 0f;
+        smoker.WithdrawalStage = 0;
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var smokerQuery = EntityQueryEnumerator<SmokerComponent>();
+        while (smokerQuery.MoveNext(out var uid, out var smoker))
+        {
+            smoker.TimeSinceSmoking += frameTime;
+
+            var adjustedInterval = smoker.SmokingInterval *(1 + smoker.WithdrawalStage);
+            if (smoker.TimeSinceSmoking < adjustedInterval)
+            {
+                continue;
+            }
+            smoker.WithdrawalStage ++;
+            //smoker.TimeSinceSmoking = 0f;
+            _popup.PopupEntity("You feel like you should smoke...",uid,uid);
+
+
+        }
+    }
+}
+
+
+
+
+
+

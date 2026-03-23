459f12801bac1d6c29f1e0fc835c1e4e30286166
CoulsTech <redaclouls@gmail.com>
2026-03-23 16:02:34 -0400

    Initial Commit with new Component to Control Tabletop object snapping

---
+++ Content.Shared/Tabletop/Components/TabletopSnapComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Shared.Tabletop.Components
+{
+    [RegisterComponent]
+    public sealed partial class TabletopSnapComponent : Component
+    {
+        public float GridSize = 1f;
+    }
+}

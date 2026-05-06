5938ee912ae392fd3bbeae8c384358e173ca9bba
ThatOneMoon <91613003+ThatOneMoon@users.noreply.github.com>
2026-05-06 22:59:30 +0300

    [Impstation port] Syndicate arms race, funkyfied (#2921)
    Co-authored-by: mq <113324899+mqole@users.noreply.github.com>
    Co-authored-by: Ruddygreat <ruddygreat1@gmail.com>
    Co-authored-by: DVDPlayerOfDiscordFame <drew.c.bergen@gmail.com>
    Co-authored-by: āda <ss.adasts@gmail.com>
    Co-authored-by: funkystationbot <funky@funkystation.org>

---
+++ Content.Server/_Impstation/FoodReagentExtractor/FoodReagentExtractorSystem.cs
@@ -0,0 +1,80 @@
+// SPDX-FileCopyrightText: 2025 āda <ss.adasts@gmail.com>
+// SPDX-FileCopyrightText: 2026 ThatOneMoon <juozas.dringelis@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+using System.Linq;
+using Content.Server.Nutrition.Components;
+using Content.Server.Popups;
+using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.FixedPoint;
+using Content.Shared.Interaction;
+using Content.Shared.Nutrition.Components;
+using Content.Shared.Whitelist;
+using Robust.Server.Audio;
+
+namespace Content.Server._Impstation.FoodReagentExtractor;
+
+// TODO the only thing keeping this system in server is food.
+/// <summary>
+///     System for extracting reagents from <see cref="FoodComponent">.
+/// </summary>
+/// <seealso cref="FoodReagentExtractorComponent"/>
+public sealed class FoodReagentExtractorSystem : EntitySystem
+{
+    [Dependency] private readonly SharedSolutionContainerSystem _solutionContainer = default!;
+    [Dependency] private readonly AudioSystem _audio = default!;
+    [Dependency] private readonly PopupSystem _popup = default!;
+    [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<FoodReagentExtractorComponent, AfterInteractUsingEvent>(OnInteractUsing);
+    }
+
+    private void OnInteractUsing(Entity<FoodReagentExtractorComponent> ent, ref AfterInteractUsingEvent args)
+    {
+        if (args.Handled || _whitelist.IsWhitelistPass(ent.Comp.EdibleBlacklist, args.Used))
+            return;
+
+        args.Handled = true;
+
+        if (!TryComp<FoodComponent>(args.Used, out var food))
+            return;
+
+        if (!_solutionContainer.TryGetSolution(args.Used, food.Solution, out var foodSol) ||
+            !_solutionContainer.TryGetSolution(ent.Owner, ent.Comp.SolutionId, out var entSol))
+            return;
+
+        // IEnumerable for reagents that match the extraction reagents
+        var reagentMatch = foodSol.Value.Comp.Solution.Contents
+                           .Where(r => ent.Comp.ExtractionReagents.Contains(r.Reagent.Prototype));
+
+        if (!reagentMatch.Any())
+        {
+            _popup.PopupEntity(Loc.GetString(ent.Comp.MessageBadFood, ("extractor", ent), ("food", args.Used)), ent, args.User);
+            return; // Food doesn't have anything we want
+        }
+
+        var total = FixedPoint2.Zero;
+        foreach (var reagent in reagentMatch)
+        {
+            _solutionContainer.TryAddReagent(entSol.Value, reagent, out var transfer);
+            total += transfer;
+        }
+
+        if (total <= 0)
+        {
+            _popup.PopupEntity(Loc.GetString(ent.Comp.MessageSolutionFull, ("extractor", ent)), ent, args.User);
+            return; // Nothing was transfered
+        }
+
+        if (ent.Comp.ExtractSound != null)
+            _audio.PlayPvs(ent.Comp.ExtractSound, ent);
+
+        _popup.PopupEntity(Loc.GetString(ent.Comp.MessageFoodEaten, ("extractor", ent), ("food", args.Used)), ent, args.User);
+        QueueDel(args.Used);
+
+        args.Handled = true;
+    }
+}
